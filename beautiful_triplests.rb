adc0426044ccbda3f4618204b1051e0dab28e9d6
Moises Hernandez <mhdezcoronado@gmail.com>
2021-06-03 21:26:00 +0200

    Add sudo code for time in words problem

--- beautiful_triplests.rb
+++ beautiful_triplests.rb
@@ -1,9 +1,9 @@
+require 'benchmark'
+
 arr = [1, 2, 4, 5, 7, 8, 10]
 # arr = [1, 6, 7, 7, 8, 10, 12, 13, 14, 19]
 d = 3
-
 def beautifulTriplets(d, arr)
-    # declare beatufull number
     beatifull = 0
     arr.each do |v|
         beatifull += 1 if arr.include?(v+d) and arr.include?(v+d+d)
@@ -11,4 +11,11 @@ def beautifulTriplets(d, arr)
     beatifull
 end
 
-p beautifulTriplets(d, arr)
+
+
+
+puts Benchmark.measure {
+    50_000.times do
+        beautifulTriplets(d, arr)
+    end
+}
---
+++ the_time_in_words.rb
@@ -0,0 +1,8 @@
+we have 3 cases:
+# when m is 00
+#     then we have o'clock
+# when m is less than or equal to 30
+#     then we have h pas the minutes
+# when m are grater than 30
+#     then we have h increasing by 1 and 60 less minutes
+
