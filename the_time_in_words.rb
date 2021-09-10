ccb883731e58d4ce7921022b8fb07244128d9baf
Moises <mhdezcoronado@gmail.com>
2021-09-10 09:44:51 +0200

    Update timeInwords

--- the_time_in_words.rb
+++ the_time_in_words.rb
@@ -6,16 +6,15 @@
 # when m are grater than 30
 #     then we have h increasing by 1 and 60 less minutes
 
-
 def timeInWords(h, m)
-    time = 
+    time =
     {
-        1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 
-        7 => 'seven', 8 => 'eight', 9 => 'nine', 10 => 'ten', 11 => 'eleven', 
-        12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'quarter', 
-        16 => 'sixtenn', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen', 
-        20 => 'twenty', 21 => 'twenty one', 22 => 'twenty two', 
-        23 => 'twenty three', 24 => 'twenty four', 25 => 'twenty five', 26 => 'twenty six', 
+        1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six',
+        7 => 'seven', 8 => 'eight', 9 => 'nine', 10 => 'ten', 11 => 'eleven',
+        12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'quarter',
+        16 => 'sixtenn', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen',
+        20 => 'twenty', 21 => 'twenty one', 22 => 'twenty two',
+        23 => 'twenty three', 24 => 'twenty four', 25 => 'twenty five', 26 => 'twenty six',
         27 => 'twenty seven', 28 => 'twenty eight', 29 => 'twenty nine', 30 => 'half'
     }
     minute = (m == 15 or m == 30 or m == 45) ? '' : m < 10 ? ' minute' : ' minutes'
@@ -31,4 +30,4 @@ def timeInWords(h, m)
     end
 end
 
-timeInWords(5, 45)
+timeInWords(5, 40)
