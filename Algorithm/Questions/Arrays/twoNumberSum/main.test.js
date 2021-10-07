302f0089943c91a7c4f5f52b56ee39562f91f86a
Moises <mhdezcoronado@gmail.com>
2021-10-07 15:08:40 +0200

    Add test for second solution

--- Algorithm/Questions/Arrays/twoNumberSum/main.test.js
+++ Algorithm/Questions/Arrays/twoNumberSum/main.test.js
@@ -1,99 +1,198 @@
-const twoNumberSum = require('./main');
+const twoNumberSum1 = require('./main');
+const twoNumberSum2 = require('./main');
 
-describe('twoNumberSum', () => {
+describe('twoNumberSum1', () => {
   it('Should return [11, -1]', () => {
     const test = {
       array: [3, 5, -4, 8, 11, 1, -1, 6],
       targetSum: 10,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([11, -1]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([11, -1]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return [4, 6]', () => {
     const test = {
       array: [4, 6],
       targetSum: 10,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([4, 6]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([4, 6]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return [4, 1]', () => {
     const test = {
       array: [4, 6, 1],
       targetSum: 5,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([4, 1]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([4, 1]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return [6, -3]', () => {
     const test = {
       array: [4, 6, 1, -3],
       targetSum: 3,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([6, -3]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([6, -3]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return [8, 9]', () => {
     const test = {
       array: [1, 2, 3, 4, 5, 6, 7, 8, 9],
       targetSum: 17,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([8, 9]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([8, 9]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return [3, 15]', () => {
     const test = {
       array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
       targetSum: 18,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([3, 15]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([3, 15]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return [-5, 0]', () => {
     const test = {
       array: [-7, -5, -3, -1, 0, 1, 3, 5, 7],
       targetSum: -5,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([-5, 0]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([-5, 0]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return [210, -47]', () => {
     const test = {
       array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47],
       targetSum: 163,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([210, -47]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([210, -47]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return []', () => {
     const test = {
       array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47],
       targetSum: 164,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return []', () => {
     const test = {
       array: [3, 5, -4, 8, 11, 1, -1, 6],
       targetSum: 15,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return []', () => {
     const test = {
       array: [14],
       targetSum: 15,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([]);
   });
 
-  it('Should return [11, -1]', () => {
+  it('Should return []', () => {
     const test = {
       array: [15],
       targetSum: 15,
     };
-    expect(twoNumberSum(test.array, test.targetSum)).toStrictEqual([]);
+    expect(twoNumberSum1(test.array, test.targetSum)).toStrictEqual([]);
+  });
+});
+
+describe('twoNumberSum2', () => {
+  it('Should return [11, -1]', () => {
+    const test = {
+      array: [3, 5, -4, 8, 11, 1, -1, 6],
+      targetSum: 10,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([11, -1]);
+  });
+
+  it('Should return [4, 6]', () => {
+    const test = {
+      array: [4, 6],
+      targetSum: 10,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([4, 6]);
+  });
+
+  it('Should return [4, 1]', () => {
+    const test = {
+      array: [4, 6, 1],
+      targetSum: 5,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([4, 1]);
+  });
+
+  it('Should return [6, -3]', () => {
+    const test = {
+      array: [4, 6, 1, -3],
+      targetSum: 3,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([6, -3]);
+  });
+
+  it('Should return [8, 9]', () => {
+    const test = {
+      array: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      targetSum: 17,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([8, 9]);
+  });
+
+  it('Should return [3, 15]', () => {
+    const test = {
+      array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
+      targetSum: 18,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([3, 15]);
+  });
+
+  it('Should return [-5, 0]', () => {
+    const test = {
+      array: [-7, -5, -3, -1, 0, 1, 3, 5, 7],
+      targetSum: -5,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([-5, 0]);
+  });
+
+  it('Should return [210, -47]', () => {
+    const test = {
+      array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47],
+      targetSum: 163,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([210, -47]);
+  });
+
+  it('Should return []', () => {
+    const test = {
+      array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47],
+      targetSum: 164,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([]);
+  });
+
+  it('Should return []', () => {
+    const test = {
+      array: [3, 5, -4, 8, 11, 1, -1, 6],
+      targetSum: 15,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([]);
+  });
+
+  it('Should return []', () => {
+    const test = {
+      array: [14],
+      targetSum: 15,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([]);
+  });
+
+  it('Should return []', () => {
+    const test = {
+      array: [15],
+      targetSum: 15,
+    };
+    expect(twoNumberSum2(test.array, test.targetSum)).toStrictEqual([]);
   });
 });
