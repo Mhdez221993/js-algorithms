d7edf28d70f2c86bccd51ad97af1b9a20303489a
Moises <mhdezcoronado@gmail.com>
2021-10-07 15:09:18 +0200

    Add second solution for twoNumberSum

--- Algorithm/Questions/Arrays/twoNumberSum/main.js
+++ Algorithm/Questions/Arrays/twoNumberSum/main.js
@@ -1,4 +1,10 @@
-function twoNumberSum(array, targetSum) {
+// array = [3, 5, -4, 8, 11, 1, -1, 6];
+// targetSum = 10;
+// currentNum = x
+// x + y = 10
+// y = 10 - x
+
+function twoNumberSum1(array, targetSum) {
   for (let i = 0; i < array.length - 1; i += 1) {
     const firstNum = array[i];
 
@@ -14,4 +20,26 @@ function twoNumberSum(array, targetSum) {
   return [];
 }
 
-module.exports = twoNumberSum;
+// twoNumberSum1 is O(n^2) T | O(1) S
+
+function twoNumberSum2(array, targetSum) {
+  const obj = {};
+
+  for (let i = 0; i < array.length; i += 1) {
+    const currentNum = array[i];
+    const lookUpNum = targetSum - currentNum;
+
+    if (obj[lookUpNum] && currentNum + lookUpNum === targetSum) {
+      return [lookUpNum, currentNum];
+    }
+
+    obj[currentNum] = true;
+  }
+
+  return [];
+}
+
+// twoNumberSum2 is O(n) T | O(n) S
+
+module.exports = twoNumberSum1;
+module.exports = twoNumberSum2;
