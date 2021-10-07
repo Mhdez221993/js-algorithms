851c2e5e607bbd16ee56b572221767d878879b8f
Moises <mhdezcoronado@gmail.com>
2021-10-07 02:47:27 +0200

    Add gitflow & eslint

--- Algorithm/Questions/Arrays/twoNumberSum/main.js
+++ Algorithm/Questions/Arrays/twoNumberSum/main.js
@@ -1,23 +1,17 @@
 function twoNumberSum(array, targetSum) {
+  for (let i = 0; i < array.length - 1; i += 1) {
+    const firstNum = array[i];
 
-	for(let i = 0; i < array.length -1; i++){
+    for (let j = i + 1; j < array.length; j += 1) {
+      const secondNum = array[j];
 
-		const firstNum = array[i];
+      if (firstNum + secondNum === targetSum) {
+        return [firstNum, secondNum];
+      }
+    }
+  }
 
-		for(let j = i + 1; j < array.length; j++){
-
-			const secondNum = array[j];
-
-			if (firstNum + secondNum === targetSum ){
-
-				return [firstNum, secondNum];
-
-			}
-		}
-	}
-
-	return []
-
+  return [];
 }
 
 module.exports = twoNumberSum;
