930d044e4c455bf0f0dbecab77b8c8479ccec911
Moises <mhdezcoronado@gmail.com>
2021-10-09 01:28:48 +0200

    Add second solution for isValidSubsequence

--- Algorithm/Questions/Arrays/ValidateSubsequence/main.js
+++ Algorithm/Questions/Arrays/ValidateSubsequence/main.js
@@ -1,13 +1,26 @@
 function isValidSubsequence(array, sequence) {
   // Write your code here.
-	let current = 0
-	array.map(v => {
-		if (v === sequence[current]) {
-			current += 1
-		}
-	})
+  let current = 0;
+  array.map(v => {
+    if (v === sequence[current]) {
+      current += 1;
+    }
+  });
 
-	return current === sequence.length
+  return current === sequence.length;
 }
+
+// function isValidSubsequence(array, sequence) {
+//  // Write your code here.
+// 	let arrIdx = 0
+// 	let seqIdx = 0
+
+// 	while(arrIdx < array.length && seqIdx < sequence.length) {
+// 		if (array[arrIdx] === sequence[seqIdx]) seqIdx += 1
+// 		arrIdx += 1
+// 	}
+
+// 	return seqIdx === sequence.length
+// }
 
 module.exports = isValidSubsequence;
--- Algorithm/Questions/Arrays/twoNumberSum/main.js
+++ Algorithm/Questions/Arrays/twoNumberSum/main.js
@@ -40,24 +40,21 @@ function twoNumberSum2(array, targetSum) {
 }
 
 function twoNumberSum(array, targetSum) {
-  array = array.sort((a,b) => a-b);
+  array = array.sort((a, b) => a - b);
 
   let left = 0;
-  let righ = array.length -1;
+  let righ = array.length - 1;
 
-  while(left < righ){
-		let currSum = array[left] + array[righ]
+  while (left < righ) {
+    const currSum = array[left] + array[righ];
 
     if (currSum === targetSum) {
-      return [array[left], array[righ]]
-
-    } else if (currSum > targetSum) {
-      righ -= 1
-
+      return [array[left], array[righ]];
+    } if (currSum > targetSum) {
+      righ -= 1;
     } else {
-      left += 1
+      left += 1;
     }
-
   }
 
   return [];
