3a35a1fd4ba71c199312085b19c560ea3a8e514c
Moises <mhdezcoronado@gmail.com>
2021-10-08 18:21:49 +0200

    Remove unnessessary code

--- Algorithm/Questions/Arrays/ValidateSubsequence/main.js
+++ Algorithm/Questions/Arrays/ValidateSubsequence/main.js
@@ -1,5 +1,17 @@
-function isValidSubsequence(array, targetSum) {
+function isValidSubsequence(array, sequence) {
+  // Write your code here.
+	let current = 0
+	array.map(v => {
+		if (v === sequence[current]) {
+			current += 1
+		}
+	})
 
+	if (current === sequence.length) {
+		return true
+	}
+
+	return false
 }
 
 module.exports = isValidSubsequence;
--- Algorithm/Questions/Arrays/ValidateSubsequence/main.test.js
+++ Algorithm/Questions/Arrays/ValidateSubsequence/main.test.js
@@ -1,5 +1,11 @@
 const isValidSubsequence = require('./main');
 
 describe('isValidSubsequence', () => {
-
+  it('', () => {
+    let test = {
+      "array": [5, 1, 22, 25, 6, -1, 8, 10],
+      "sequence": [1, 6, -1, 10]
+    }
+    expect(isValidSubsequence(test.array, test.sequence)).toBeTruthy()
+  })
 });
