21f69a130c59c1be5665d851cba4c79db687d04f
Moises <mhdezcoronado@gmail.com>
2021-10-11 16:42:38 +0200

    Add second solution for tournamentWinner

--- Algorithm/Questions/Arrays/jscript/tournamentWinner/main.js
+++ Algorithm/Questions/Arrays/jscript/tournamentWinner/main.js
@@ -1,20 +1,25 @@
+const HOME_TEAM_WON = 1
+
 function tournamentWinner(competitions, results) {
   // Write your code here.
-  let hightest = 0
-  let winner = ''
-  let hash = {}
+  let currentBestTeam = ''
+  let scores = {[currentBestTeam]: 0}
 
   for (let i = 0; i < results.length; i++) {
-    let currIdx = results[i] === 1 ? 0 : 1
-    let curr = competitions[i][currIdx]
 
-    hash[curr] = hash[curr]+3 || 3
 
-    winner = hash[curr] > hightest ? curr : winner
-    hightest = hash[curr] > hightest ? hash[curr] : hightest
+    let result = results[i]
+    let [homeTeam, awayTeam] = competitions[i]
+    let winingTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam
+
+    scores[winingTeam] = scores[winingTeam] + 3 || 3
+
+    if (scores[winingTeam] > scores[currentBestTeam]) {
+      currentBestTeam = winingTeam
+    }
   }
 
-  return winner;
+  return currentBestTeam;
 }
 
 module.exports = tournamentWinner;
