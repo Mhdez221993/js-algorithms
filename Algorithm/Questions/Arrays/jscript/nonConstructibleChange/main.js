78c7842398d5e8b3f13b6007cd6407a14bc37042
Moises <mhdezcoronado@gmail.com>
2021-10-12 10:49:09 +0200

    Add first solution for nonConstructibleChange

--- Algorithm/Questions/Arrays/jscript/nonConstructibleChange/main.js
+++ Algorithm/Questions/Arrays/jscript/nonConstructibleChange/main.js
@@ -1,6 +1,19 @@
 function nonConstructibleChange(coins) {
-  // Write your code here.
-  return 1;
+
+  coins.sort((a,b) => a-b)
+
+  let currentChange = 0
+  coins.map(v => {
+    if(v > currentChange+1) return currentChange+1
+    currentChange += v
+  })
+
+
+  return currentChange+1;
 }
 
-module.exports = nonConstructibleChange;
+let coins = [6, 4, 5, 1, 1, 8, 9]
+
+console.log(nonConstructibleChange(coins));
+
+// module.exports = nonConstructibleChange;
