4c8baabd7f18b42af044d98d2d85f70c2742b6fb
Moises <mhdezcoronado@gmail.com>
2021-10-14 11:53:45 +0200

    Add solution and test for threeNumberSum

--- Algorithm/Questions/Arrays/jscript/threeNumberSum/main.js
+++ Algorithm/Questions/Arrays/jscript/threeNumberSum/main.js
@@ -1,6 +1,32 @@
 function threeNumberSum(array, targetSum) {
   // Write your code here.
-}
+  array.sort((a,b) => a-b)
 
+  let triples = []
+
+  for (let i = 0; i < array.length -1; i++) {
+    let left = i+1
+    let right = array.length-1
+
+    while(right > left){
+      let currSum = array[i] + array[left] + array[right]
+
+      if (currSum > targetSum) {
+        right--
+
+      } else if (currSum < targetSum) {
+        left++
+
+      } else {
+        triples.push([array[i], array[left], array[right]])
+        left++
+        right--
+      }
+    }
+
+  }
+
+  return triples
+}
 
 module.exports = threeNumberSum;
--- Algorithm/Questions/Arrays/jscript/threeNumberSum/main.test.js
+++ Algorithm/Questions/Arrays/jscript/threeNumberSum/main.test.js
@@ -6,8 +6,12 @@ describe('threeNumberSum', () => {
       "array": [12, 3, 1, 2, -6, 5, -8, 6],
       "targetSum": 0
     };
-    let result = 0
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = [
+      [-8, 2, 6],
+      [-8, 3, 5],
+      [-6, 1, 5]
+    ]
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 
   it('should return true', () => {
@@ -15,8 +19,10 @@ describe('threeNumberSum', () => {
       "array": [1, 2, 3],
       "targetSum": 6
     };
-    let result = 0
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = [
+      [1, 2, 3]
+    ]
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 
   it('should return true', () => {
@@ -24,8 +30,8 @@ describe('threeNumberSum', () => {
       "array": [1, 2, 3],
       "targetSum": 7
     };
-    let result = 55
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = []
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 
   it('should return true', () => {
@@ -33,8 +39,10 @@ describe('threeNumberSum', () => {
       "array": [8, 10, -2, 49, 14],
       "targetSum": 57
     };
-    let result = 3
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = [
+      [-2, 10, 49]
+    ]
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 
   it('should return true', () => {
@@ -42,8 +50,12 @@ describe('threeNumberSum', () => {
       "array": [12, 3, 1, 2, -6, 5, 0, -8, -1],
       "targetSum": 0
     };
-    let result = 1
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = [
+      [-8, 3, 5],
+      [-6, 1, 5],
+      [-1, 0, 1]
+    ]
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 
   it('should return true', () => {
@@ -51,8 +63,14 @@ describe('threeNumberSum', () => {
       "array": [12, 3, 1, 2, -6, 5, 0, -8, -1, 6],
       "targetSum": 0
     };
-    let result = 1
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = [
+      [-8, 2, 6],
+      [-8, 3, 5],
+      [-6, 0, 6],
+      [-6, 1, 5],
+      [-1, 0, 1]
+    ]
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 
   it('should return true', () => {
@@ -60,8 +78,17 @@ describe('threeNumberSum', () => {
       "array": [12, 3, 1, 2, -6, 5, 0, -8, -1, 6, -5],
       "targetSum": 0
     };
-    let result = 32
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = [
+      [-8, 2, 6],
+      [-8, 3, 5],
+      [-6, 0, 6],
+      [-6, 1, 5],
+      [-5, -1, 6],
+      [-5, 0, 5],
+      [-5, 2, 3],
+      [-1, 0, 1]
+    ]
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 
   it('should return true', () => {
@@ -69,8 +96,17 @@ describe('threeNumberSum', () => {
       "array": [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
       "targetSum": 18
     };
-    let result = 19
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = [
+      [1, 2, 15],
+      [1, 8, 9],
+      [2, 7, 9],
+      [3, 6, 9],
+      [3, 7, 8],
+      [4, 5, 9],
+      [4, 6, 8],
+      [5, 6, 7]
+    ]
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 
   it('should return true', () => {
@@ -78,8 +114,10 @@ describe('threeNumberSum', () => {
       "array": [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
       "targetSum": 32
     };
-    let result = 3
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = [
+      [8, 9, 15]
+    ]
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 
   it('should return true', () => {
@@ -87,8 +125,8 @@ describe('threeNumberSum', () => {
       "array": [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
       "targetSum": 33
     };
-    let result = 1
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = []
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 
   it('should return true', () => {
@@ -96,7 +134,7 @@ describe('threeNumberSum', () => {
       "array": [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
       "targetSum": 5
     };
-    let result = 2
-    expect(threeNumberSum(test.array, test.targetSum)).toBe(result);
+    let result = []
+    expect(threeNumberSum(test.array, test.targetSum)).toStrictEqual(result);
   });
 });
