38c9553297fa45cbd182fd07e2aed68a465a73d0
Moises <mhdezcoronado@gmail.com>
2021-10-16 00:24:07 +0200

    Add test cases for moveElementToEnd

--- Algorithm/Questions/Arrays/jscript/moveElementToEnd/main.test.js
+++ Algorithm/Questions/Arrays/jscript/moveElementToEnd/main.test.js
@@ -1,93 +1,102 @@
 const moveElementToEnd = require('./main');
 
 describe('moveElementToEnd', () => {
-  it('should return true', () => {
+  it('should return result', () => {
     const test = {
-      "arrayOne": [-1, 5, 10, 20, 28, 3],
-      "arrayTwo": [26, 134, 135, 15, 17]
+      "array": [2, 1, 2, 2, 2, 3, 4, 2],
+      "toMove": 2
     };
     let result = [28, 26]
-    expect(moveElementToEnd(test.arrayOne, test.arrayTwo)).toStrictEqual(result);
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
   });
 
-  it('should return true', () => {
+  it('should return result', () => {
     const test = {
-      "arrayOne": [-1, 5, 10, 20, 3],
-      "arrayTwo": [26, 134, 135, 15, 17]
+      "array": [],
+      "toMove": 3
     };
     let result = [20, 17]
-    expect(moveElementToEnd(test.arrayOne, test.arrayTwo)).toStrictEqual(result);
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
   });
 
-  it('should return true', () => {
+  it('should return result', () => {
     const test = {
-      "arrayOne": [10, 0, 20, 25],
-      "arrayTwo": [1005, 1006, 1014, 1032, 1031]
+      "array": [1, 2, 4, 5, 6],
+      "toMove": 3
     };
     let result = [25, 1005]
-    expect(moveElementToEnd(test.arrayOne, test.arrayTwo)).toStrictEqual(result);
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
   });
 
-  it('should return true', () => {
-    const test = {
-      "arrayOne": [10, 0, 20, 25, 2200],
-      "arrayTwo": [1005, 1006, 1014, 1032, 1031]
+  it('should return result', () => {
+    const test ={
+      "array": [3, 3, 3, 3, 3],
+      "toMove": 3
     };
     let result = [25, 1005]
-    expect(moveElementToEnd(test.arrayOne, test.arrayTwo)).toStrictEqual(result);
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
   });
 
-  it('should return true', () => {
-    const test = {
-      "arrayOne": [10, 0, 20, 25, 2000],
-      "arrayTwo": [1005, 1006, 1014, 1032, 1031]
+  it('should return result', () => {
+    const test ={
+      "array": [3, 1, 2, 4, 5],
+      "toMove": 3
     };
     let result = [2000, 1032]
-    expect(moveElementToEnd(test.arrayOne, test.arrayTwo)).toStrictEqual(result);
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
   });
 
-  it('should return true', () => {
+  it('should return result', () => {
     const test = {
-      "arrayOne": [240, 124, 86, 111, 2, 84, 954, 27, 89],
-      "arrayTwo": [1, 3, 954, 19, 8]
+      "array": [1, 2, 4, 5, 3],
+      "toMove": 3
     };
     let result = [954, 954]
-    expect(moveElementToEnd(test.arrayOne, test.arrayTwo)).toStrictEqual(result);
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
   });
 
-  it('should return true', () => {
+  it('should return result', () => {
     const test = {
-      "arrayOne": [0, 20],
-      "arrayTwo": [21, -2]
+      "array": [1, 2, 3, 4, 5],
+      "toMove": 3
     };
     let result = [20, 21]
-    expect(moveElementToEnd(test.arrayOne, test.arrayTwo)).toStrictEqual(result);
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
   });
 
-  it('should return true', () => {
+  it('should return result', () => {
     const test = {
-      "arrayOne": [10, 1000],
-      "arrayTwo": [-1441, -124, -25, 1014, 1500, 660, 410, 245, 530]
+      "array": [2, 4, 2, 5, 6, 2, 2],
+      "toMove": 2
     };
     let result = [1000, 1014]
-    expect(moveElementToEnd(test.arrayOne, test.arrayTwo)).toStrictEqual(result);
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
   });
 
-  it('should return true', () => {
+  it('should return result', () => {
     const test = {
-      "arrayOne": [10, 1000, 9124, 2142, 59, 24, 596, 591, 124, -123],
-      "arrayTwo": [-1441, -124, -25, 1014, 1500, 660, 410, 245, 530]
+      "array": [5, 5, 5, 5, 5, 5, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12],
+      "toMove": 5
     };
     let result = [-123, -124]
-    expect(moveElementToEnd(test.arrayOne, test.arrayTwo)).toStrictEqual(result);
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
   });
 
-  it('should return true', () => {
+  it('should return result', () => {
     const test = {
-      "arrayOne": [10, 1000, 9124, 2142, 59, 24, 596, 591, 124, -123, 530],
-      "arrayTwo": [-1441, -124, -25, 1014, 1500, 660, 410, 245, 530]
+      "array": [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 5, 5, 5, 5, 5, 5],
+      "toMove": 5
     };
     let result = [530, 530]
-    expect(moveElementToEnd(test.arrayOne, test.arrayTwo)).toStrictEqual(result);
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [5, 1, 2, 5, 5, 3, 4, 6, 7, 5, 8, 9, 10, 11, 5, 5, 12],
+      "toMove": 5
+    };
+    let result = [530, 530]
+    expect(moveElementToEnd(test.array, test.toMove)).toStrictEqual(result);
   });
 });
