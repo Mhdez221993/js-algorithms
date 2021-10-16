6fc889997ca15e111bcd9112379cca4fc4fb51b6
Moises <mhdezcoronado@gmail.com>
2021-10-17 01:45:52 +0200

    Add first solution for isMonotonic

--- Algorithm/Questions/Arrays/jscript/isMonotonic/main.js
+++ Algorithm/Questions/Arrays/jscript/isMonotonic/main.js
@@ -1,6 +1,34 @@
 function isMonotonic(array) {
-  // Write your code here.
+
+  if (!array.length || array.length === 1) {
+    return true
+  }
+
+  let i = 0
+  let index = 1
+  while(array[i] === array[i+1]) {
+    index++
+    i++
+  }
+
+  const direction = array[index] > array[index-1] ? 'Increase' : 'Decrease'
+  for(let i = index; i < array.length; i++){
+    let currentValue = array[i]
+    let previosValue = array[i-1]
+
+    if (direction === 'Increase' && currentValue < previosValue) {
+      return false
+
+    } else if (direction === 'Decrease' && currentValue > previosValue) {
+      return false
+    }
+  }
+
+  return true
 }
+
+// let array = [-1, -1, -2, -3, -4, -5, -5, -5, -6, -7, -8, -8, -9, -10, -11]
+// console.log(isMonotonic(array));
 
 module.exports = isMonotonic;
 
