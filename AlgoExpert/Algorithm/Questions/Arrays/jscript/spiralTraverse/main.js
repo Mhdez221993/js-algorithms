5cdb3d57b7074d7e430c2921096ca201e5b825a0
Moises <mhdezcoronado@gmail.com>
2021-10-18 00:38:43 +0200

    Add first setup

---
+++ AlgoExpert/Algorithm/Questions/Arrays/jscript/spiralTraverse/main.js
@@ -0,0 +1,6 @@
+function spiralTraverse(array) {
+  // Write your code here.
+}
+
+
+module.exports = spiralTraverse;
---
+++ AlgoExpert/Algorithm/Questions/Arrays/jscript/spiralTraverse/main.test.js
@@ -0,0 +1,91 @@
+const spiralTraverse = require('./main');
+
+describe('spiralTraverse', () => {
+  it('should return result', () => {
+    const test = {
+      "array": [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+    };
+    let result = true
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": []
+    };
+    let result = true
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [1]
+    };
+    let result = true
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [1, 2]
+    };
+    let result = true
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [2, 1]
+    };
+    let result = true
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [1, 5, 10, 1100, 1101, 1102, 9001]
+    };
+    let result = true
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [-1, -5, -10, -1100, -1101, -1102, -9001]
+    };
+    let result = true
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [-1, -5, -10, -1100, -900, -1101, -1102, -9001]
+    };
+    let result = false
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [1, 2, 0]
+    };
+    let result = false
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 7, 9, 10, 11]
+    };
+    let result = false
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 11]
+    };
+    let result = true
+    expect(spiralTraverse(test.array)).toStrictEqual(result);
+  });
+});
