ad9909a871a870274a76660993ef3d144bf5e1af
Moises <mhdezcoronado@gmail.com>
2021-10-17 17:00:43 +0200

    Add third solution for isMonotonic Question

--- Algorithm/Questions/Arrays/jscript/isMonotonic/main.js
+++ Algorithm/Questions/Arrays/jscript/isMonotonic/main.js
@@ -1,38 +1,21 @@
 // O(n) Time | O(1) Space
-function breakDirection(direction, prev, curr){
-  let diff = curr - prev
-
-  if (direction > 0) {
-    return diff < 0
-  }
-
-  return diff > 0
-}
-
 function isMonotonic(array) {
 
-  if (array.length <= 2) {
-    return true
-  }
+  let isNonIncreaseaing = true
+  let isNonDecreaseaing = true
 
-  let direction = array[1] - array[0]
-  for(let i = 2; i < array.length; i++){
+  for(let i = 1; i < array.length; i++) {
 
-    if(direction === 0) {
-      direction === array[i] - array[i-1]
-      continue
+    if (array[i] > array[i - 1]) {
+      isNonDecreaseaing = false
     }
 
-    if(breakDirection(direction, array[i -1], array[i])) {
-      return false
+    if (array[i] < array[i -1]) {
+      isNonIncreaseaing = false
     }
   }
 
-  return true
+  return isNonDecreaseaing || isNonIncreaseaing
 }
 
-let array = [1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 7, 9, 10, 11]
-console.log(isMonotonic(array));
-
 module.exports = isMonotonic;
-
