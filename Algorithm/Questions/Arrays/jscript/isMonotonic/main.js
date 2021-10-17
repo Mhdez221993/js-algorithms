05077ea44782a79fe2d6b64813450ded02d786f0
Moises <mhdezcoronado@gmail.com>
2021-10-17 16:47:25 +0200

    Add second solution for breakDirection

--- Algorithm/Questions/Arrays/jscript/isMonotonic/main.js
+++ Algorithm/Questions/Arrays/jscript/isMonotonic/main.js
@@ -1,25 +1,29 @@
+// O(n) Time | O(1) Space
+function breakDirection(direction, prev, curr){
+  let diff = curr - prev
+
+  if (direction > 0) {
+    return diff < 0
+  }
+
+  return diff > 0
+}
+
 function isMonotonic(array) {
 
-  if (!array.length || array.length === 1) {
+  if (array.length <= 2) {
     return true
   }
 
-  let i = 0
-  let index = 1
-  while(array[i] === array[i+1]) {
-    index++
-    i++
-  }
+  let direction = array[1] - array[0]
+  for(let i = 2; i < array.length; i++){
 
-  const direction = array[index] > array[index-1] ? 'Increase' : 'Decrease'
-  for(let i = index; i < array.length; i++){
-    let currentValue = array[i]
-    let previosValue = array[i-1]
+    if(direction === 0) {
+      direction === array[i] - array[i-1]
+      continue
+    }
 
-    if (direction === 'Increase' && currentValue < previosValue) {
-      return false
-
-    } else if (direction === 'Decrease' && currentValue > previosValue) {
+    if(breakDirection(direction, array[i -1], array[i])) {
       return false
     }
   }
@@ -27,8 +31,8 @@ function isMonotonic(array) {
   return true
 }
 
-// let array = [-1, -1, -2, -3, -4, -5, -5, -5, -6, -7, -8, -8, -9, -10, -11]
-// console.log(isMonotonic(array));
+let array = [1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 7, 9, 10, 11]
+console.log(isMonotonic(array));
 
 module.exports = isMonotonic;
 
