18a7cb76fd71d92dede16df70901844c0d461a86
Moises <mhdezcoronado@gmail.com>
2021-10-18 23:37:08 +0200

    Add first solution for spiralTraverse

--- Algo/Algorithm/Questions/Arrays/spiralTraverse/main.js
+++ Algo/Algorithm/Questions/Arrays/spiralTraverse/main.js
@@ -1,16 +1,48 @@
 function spiralTraverse(array) {
-  if (array.length === 1) {
-    return array[0]
+  let spiralArray = []
+  let startRow = 0
+  let startCol = 0
+  let endRow = array.length - 1
+  let endCol = array[0].length - 1
+
+
+  while(startRow <= endRow && startCol <= endCol) {
+
+    for(let col = startCol; col <= endCol; col++){
+      spiralArray.push(array[startRow][col])
+    }
+
+    for(let row = startRow + 1; row <= endRow; row++){
+
+      spiralArray.push(array[row][endCol])
+    }
+
+    for(let col = endCol - 1; col >= startCol; col--){
+
+      if(startCol === endRow) break
+      spiralArray.push(array[endRow][col])
+    }
+
+    for(let row = endRow - 1; row > startRow; row--){
+
+      if(startCol === endCol) break
+      spiralArray.push(array[row][startCol])
+    }
+
+    startRow++
+    endRow--
+    startCol++
+    endCol--
   }
 
-  let len = array.length
+  return spiralArray
 }
 
 let array = [
   [1, 2, 3, 4],
-  [12, 13, 14, 5]
-  // [11, 16, 15, 6],
-  // [10, 9, 8, 7]
+  [12, 13, 14, 5],
+  [11, 16, 15, 6],
+  [10, 9, 8, 7]
 ]
 
 console.log(spiralTraverse(array));
