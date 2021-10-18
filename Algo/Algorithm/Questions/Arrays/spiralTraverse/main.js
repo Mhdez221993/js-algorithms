156f418f31bb5d30e6e62918ed74695d50fa05ba
Moises <mhdezcoronado@gmail.com>
2021-10-19 01:06:36 +0200

    Add second solution for spiralTraverse

--- Algo/Algorithm/Questions/Arrays/spiralTraverse/main.js
+++ Algo/Algorithm/Questions/Arrays/spiralTraverse/main.js
@@ -1,36 +1,69 @@
+// function spiralTraverse(array) {
+//   let result = []
+//   let startRow = 0
+//   let endRow = array.length - 1
+//   let startCol = 0
+//   let endCol = array[0].length - 1
+
+//   while(startRow <= endRow && startCol <= endCol){
+//     for(let col = startCol; col <= endCol; col++){
+//       result.push(array[startRow][col])
+//     }
+
+//     for(let row = startRow + 1; row <= endRow; row++){
+//       result.push(array[row][endCol])
+//     }
+
+//     for(let col = endCol - 1; col >= startCol; col--){
+//       if(startRow === endRow) break
+//       result.push(array[endRow][col])
+//     }
+
+//     for(let row = endRow - 1; row > startRow; row--){
+//       if(startCol === endCol) break
+//       result.push(array[row][startCol])
+//     }
+
+//     startRow++
+//     startCol++
+//     endRow--
+//     endCol--
+//   }
+
+//   return result
+// }
+
 function spiralTraverse(array) {
   let result = []
-  let startRow = 0
-  let endRow = array.length - 1
-  let startCol = 0
-  let endCol = array[0].length - 1
+  recursiveSpiral(array, 0, array.length - 1, 0, array[0].length - 1, result)
+  return result
+}
 
-  while(startRow <= endRow && startCol <= endCol){
-    for(let col = startCol; col <= endCol; col++){
-      result.push(array[startRow][col])
-    }
+function recursiveSpiral(array, startRow, endRow, startCol, endCol, result){
 
-    for(let row = startRow + 1; row <= endRow; row++){
-      result.push(array[row][endCol])
-    }
-
-    for(let col = endCol - 1; col >= startCol; col--){
-      if(startRow === endRow) break
-      result.push(array[endRow][col])
-    }
-
-    for(let row = endRow - 1; row > startRow; row--){
-      if(startCol === endCol) break
-      result.push(array[row][startCol])
-    }
-
-    startRow++
-    startCol++
-    endRow--
-    endCol--
+  if(startRow > endRow || startCol > endCol){
+    return
   }
 
-  return result
+  for(let col = startCol; col <= endCol; col++){
+    result.push(array[startRow][col])
+  }
+
+  for(let row = startRow + 1; row <= endRow; row++){
+    result.push(array[row][endCol])
+  }
+
+  for(let col = endCol - 1; col >= startCol; col--){
+    if(startRow === endRow) break
+    result.push(array[endRow][col])
+  }
+
+  for(let row = endRow - 1; row > startRow; row--){
+    if(startCol === endCol) break
+    result.push(array[row][startCol])
+  }
+
+  recursiveSpiral(array, startRow+1, endRow-1, startCol+1, endCol-1, result)
 }
 
 module.exports = spiralTraverse;
