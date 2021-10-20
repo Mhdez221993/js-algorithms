c80870fea5dfb498cb759524bdaebb8c70ed1105
Moises <mhdezcoronado@gmail.com>
2021-10-20 14:32:38 +0200

    Print Hello with Java

---
+++ GFG/ProgrammingLanguage/Java/PrintHello.java
@@ -0,0 +1,23 @@
+class Geeks{
+
+  // Function to print hello
+  static void printHelloWorld(){
+
+      System.out.print("Hello World\n");
+
+  }
+
+}
+
+class PrintHello{
+
+    // Function to print hello
+    public static void main(String args[]){
+
+        Geeks g = new Geeks();
+
+        g.printHelloWorld();
+
+    }
+
+}
