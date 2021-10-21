c33dcf4b7caf51cba924e9e4f7a7bbf7452bfdc7
Moises <mhdezcoronado@gmail.com>
2021-10-21 02:05:39 +0200

    Add first solution for longestPeak

--- Algo/Algorithm/Questions/Arrays/longestPeak/main.js
+++ Algo/Algorithm/Questions/Arrays/longestPeak/main.js
@@ -1,25 +1,38 @@
 function longestPeak(array) {
-  let currentPeak = 0
-  let previosPeak = 0
+  let currentPeak = 1
+  let hightestPeak = 0
   let peak = false
+  let atleatOnePeak = false;
 
   for(let i = 1; i < array.length; i++) {
     let curr = array[i]
     let prev = array[i - 1]
 
-    if (curr > prev && currentPeak === 0) {
+    if(curr > prev) {
 
+      if(peak){
+        currentPeak = 2
+        peak = false
+
+      } else {
+        currentPeak++
+      }
+
+    } else if(curr < prev && currentPeak > 1) {
       currentPeak++
+      peak = true
+      atleatOnePeak = true
 
-    } else if(curr > prev) {
-      currentPeak = 2
     } else {
-      currentPeak = 0
+      currentPeak = 1
     }
+
+    hightestPeak = currentPeak > hightestPeak ? currentPeak : hightestPeak
   };
 
-  return currentPeak
+  return atleatOnePeak ? hightestPeak : 0
 }
+
 let array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
 console.log(longestPeak(array));
 // module.exports = spiralTraverse;
