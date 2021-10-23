455c8941dd6cf2f078a90e8500936a9342fe84ee
Moises <mhdezcoronado@gmail.com>
2021-10-23 17:18:40 +0200

    Add third solution for arrayOfProducts

--- Algo/Algorithm/Questions/Arrays/arrayOfProducts/main.js
+++ Algo/Algorithm/Questions/Arrays/arrayOfProducts/main.js
@@ -1,22 +1,16 @@
 function arrayOfProducts(array) {
-  let products = []
-  let rigthProduct = []
-  let leftProduct = []
+  let products = new Array(array.length).fill(1)
 
   let leftCurrentProduct = 1
   for(let i = 0; i < array.length; i++){
-    leftProduct[i] = leftCurrentProduct
+    products[i] *= leftCurrentProduct
     leftCurrentProduct *= array[i]
   }
 
   let rigthCurrentProduct = 1
   for(let i = array.length - 1; i >= 0; i--){
-    rigthProduct[i] = rigthCurrentProduct
+    products[i] *= rigthCurrentProduct
     rigthCurrentProduct *= array[i]
-  }
-
-  for(let i = 0; i < array.length; i++){
-    products[i] = rigthProduct[i] * leftProduct[i]
   }
 
   return products
