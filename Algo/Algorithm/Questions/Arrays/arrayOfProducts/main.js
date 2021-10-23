8ef6d1de4f98e4af52a77de1168908005c835dfb
Moises <mhdezcoronado@gmail.com>
2021-10-23 02:03:36 +0200

    Add first solution for arrayOfProducts

--- Algo/Algorithm/Questions/Arrays/arrayOfProducts/main.js
+++ Algo/Algorithm/Questions/Arrays/arrayOfProducts/main.js
@@ -1,5 +1,23 @@
 function arrayOfProducts(array) {
-  // Write your code here.
+  let newArr = []
+
+  for(let i = 0; i < array.length; i++){
+    let product = 1
+    for(let j = 0; j < array.length; j++){
+
+      if(j === i){
+        continue
+      }
+
+      product *= array[j]
+    }
+
+    newArr.push(product)
+  }
+
+  return newArr
 }
 
-module.exports = arrayOfProducts;
+let array = [5, 1, 4, 2]
+console.log(arrayOfProducts(array));
+// module.exports = arrayOfProducts;
