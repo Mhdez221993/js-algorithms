bdefade745c63d3738091390ff8dc3e68c908788
Moises <mhdezcoronado@gmail.com>
2021-10-27 13:11:40 +0200

    Add test for firstDuplicateValue

--- Algo/Algorithm/Questions/Arrays/firstDuplicateValue/main.test.js
+++ Algo/Algorithm/Questions/Arrays/firstDuplicateValue/main.test.js
@@ -9,91 +9,91 @@ describe('firstDuplicateValue', () => {
     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
   });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array": []
-//     };
-//     let result = 0
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array": [2, 1, 5, 3, 3, 2, 4]
+    };
+    let result = 3
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array": [1, 3, 2]
-//     };
-//     let result = 3
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array": [1, 1, 2, 3, 3, 2, 2]
+    };
+    let result = 1
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array":[1, 2, 3, 4, 5, 1]
-//     }
-//     let result = 6
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array": [3, 1, 3, 1, 1, 4, 4]
+    }
+    let result = 3
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array": [5, 4, 3, 2, 1, 2, 1]
-//     };
-//     let result = 3
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array": []
+    };
+    let result = -1
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array":[5, 4, 3, 2, 1, 2, 10, 12, -3, 5, 6, 7, 10]
-//     };
-//     let result = 5
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array":[1]
+    };
+    let result = -1
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array": [5, 4, 3, 2, 1, 2, 10, 12]
-//     };
-//     let result = 0
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array": [1, 1]
+    };
+    let result = 1
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array": [1, 2, 3, 4, 5, 6, 10, 100, 1000]
-//     };
-//     let result = 0
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10]
+    };
+    let result = 10
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array": [1, 2, 3, 3, 2, 1]
-//     };
-//     let result = 0
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array": [2, 1, 1]
+    };
+    let result = 1
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array": [1, 1, 3, 2, 1]
-//     };
-//     let result = 4
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array": [2, 2, 2, 2, 2, 2, 2, 2, 2]
+    };
+    let result = 2
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array": [1, 2, 3, 2, 1, 1]
-//     };
-//     let result = 5
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array": [6, 15, 7, 10, 9, 14, 10, 1, 10, 1, 2, 11, 1, 6, 8]
+    };
+    let result = 10
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 
-//   it('should return result', () => {
-//     const test = {
-//       "array": [1, 1, 1, 2, 3, 10, 12, -3, -3, 2, 3, 45, 800, 99, 98, 0, -1, -1, 2, 3, 4, 5, 0, -1, -1]
-//     };
-//     let result = 9
-//     expect(firstDuplicateValue(test.array)).toStrictEqual(result);
-//   });
+  it('should return result', () => {
+    const test = {
+      "array": [13, 4, 10, 10, 8, 13, 13, 7, 11, 6, 3, 2, 11]
+    };
+    let result = 10
+    expect(firstDuplicateValue(test.array)).toStrictEqual(result);
+  });
 });
