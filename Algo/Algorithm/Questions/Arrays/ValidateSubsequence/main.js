67c29b10caed10dbc56a881e7b03030dc97618a0
Moises <mhdezcoronado@gmail.com>
2021-11-01 18:03:46 +0100

    Remove unnessessary code

--- Algo/Algorithm/Questions/Arrays/ValidateSubsequence/main.js
+++ Algo/Algorithm/Questions/Arrays/ValidateSubsequence/main.js
@@ -1,6 +1,5 @@
 // O(n) time | O(1) space
 function isValidSubsequence(array, sequence) {
-  // Write your code here.
   let current = 0;
   array.map(v => {
 		if (current === sequence.length) {
@@ -14,19 +13,5 @@ function isValidSubsequence(array, sequence) {
 
   return current === sequence.length;
 }
-
-// O(n) time | O(1) space
-// function isValidSubsequence(array, sequence) {
-//  // Write your code here.
-// 	let arrIdx = 0
-// 	let seqIdx = 0
-
-// 	while(arrIdx < array.length && seqIdx < sequence.length) {
-// 		if (array[arrIdx] === sequence[seqIdx]) seqIdx += 1
-// 		arrIdx += 1
-// 	}
-
-// 	return seqIdx === sequence.length
-// }
 
 module.exports = isValidSubsequence;
