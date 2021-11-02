c7a9d6a1491e6310394cefee67df144b30979884
Moises <mhdezcoronado@gmail.com>
2021-11-02 14:59:21 +0100

    Add final solution for mergeOverlappingIntervals

--- Algo/Algorithm/Questions/Arrays/mergeOverlappingIntervals/main.js
+++ Algo/Algorithm/Questions/Arrays/mergeOverlappingIntervals/main.js
@@ -1,50 +1,23 @@
 function mergeOverlappingIntervals(array) {
   array = array.sort(([a],[b]) => a - b)
   let merge = [];
-  let currFirst = undefined;
-  let currSecond = undefined;
-  let [lastFirst, lastSecond] = array[array.length -1]
-  for(let i = 0; i < array.length - 1; i++) {
-    let [first, second] = array[i];
-    let [firstNext, secondNext] = array[i+1];
+  let currentInterval = array[0]
+	merge.push(currentInterval)
 
-    first = currFirst ? currFirst : first
-    second = currSecond ? currSecond : second
-    if (second < firstNext) {
-      merge.push([first, second])
-      currFirst = undefined
-      currSecond = undefined
-    } else if (second > secondNext) {
-      currSecond = second
+  for(const nextInterval of array) {
+    let [_, second] = currentInterval;
+    let [firstNext, secondNext] = nextInterval;
+
+    if (second >= firstNext) {
+      currentInterval[1] = Math.max(second, secondNext)
+
     } else {
-      currFirst = first
+			currentInterval = nextInterval
+      merge.push(nextInterval)
     }
   }
-
-  lastFirst = currFirst ? currFirst : lastFirst
-  // lastSecond = currSecond ? currSecond : lastFirst
-  merge.push([lastFirst, lastSecond])
-
 
   return merge;
 }
 
-let array = [
-  [1, 2],
-  [3, 5],
-  [4, 7],
-  [6, 8],
-  [9, 10]
-]
-
-let result = [
-  [1, 2],
-  [3, 5],
-  [4, 7],
-  [6, 8],
-  [9, 10]
-]
-
-console.log(mergeOverlappingIntervals(array));
-
-// module.exports = mergeOverlappingIntervals;
+module.exports = mergeOverlappingIntervals;
