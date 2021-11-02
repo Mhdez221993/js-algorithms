59a2648e3014d80b3b34862a1fdbd4448d5df07e
Moises <mhdezcoronado@gmail.com>
2021-11-02 01:57:50 +0100

    mergeOverlappingIntervals for sorted arrays

--- Algo/Algorithm/Questions/Arrays/mergeOverlappingIntervals/main.js
+++ Algo/Algorithm/Questions/Arrays/mergeOverlappingIntervals/main.js
@@ -1,6 +1,41 @@
 function mergeOverlappingIntervals(array) {
-  // Write your code here.
-  return [[]];
+  let merge = [];
+  let curr = undefined;
+  let [lastFirst, lastSecond] = array[array.length -1]
+  for(let i = 0; i < array.length - 1; i++) {
+    let [first, second] = array[i];
+    let [firstNext, secondNext] = array[i+1];
+
+    first = curr ? curr : first
+    if (second < firstNext) {
+      merge.push([first, second])
+      curr = undefined
+    } else {
+      curr = first
+    }
+  }
+
+  lastFirst = curr ? curr : lastFirst
+  merge.push([lastFirst, lastSecond])
+
+
+  return merge;
 }
 
-module.exports = mergeOverlappingIntervals;
+let array = [
+  [1, 2],
+  [3, 5],
+  [4, 7],
+  [6, 8],
+  [9, 10]
+]
+
+let result = [
+  [1, 2],
+  [3, 8],
+  [9, 10]
+]
+
+console.log(mergeOverlappingIntervals(array));
+
+// module.exports = mergeOverlappingIntervals;
