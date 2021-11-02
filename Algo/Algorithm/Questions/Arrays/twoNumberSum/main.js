7aa24374cd379d049c0aa0137f0aac316b689721
Moises <mhdezcoronado@gmail.com>
2021-11-02 08:43:04 +0100

    Review twoNumberSum challenge

--- Algo/Algorithm/Questions/Arrays/twoNumberSum/main.js
+++ Algo/Algorithm/Questions/Arrays/twoNumberSum/main.js
@@ -1,22 +1,24 @@
 function twoNumberSum(array, targetSum) {
-  array = array.sort((a, b) => a - b);
+  array.sort((a, b) => a-b)
 
-  let left = 0;
-  let righ = array.length - 1;
+  let left = 0
+  let right = array.length - 1;
 
-  while (left < righ) {
-    const currSum = array[left] + array[righ];
+  while(left < right) {
+    let current = array[left] + array[right]
 
-    if (currSum === targetSum) {
-      return [array[left], array[righ]];
-    } if (currSum > targetSum) {
-      righ -= 1;
+    if (current === targetSum){
+      return [array[left], array[right]]
+
+    } else if (current > targetSum) {
+      right--
+
     } else {
-      left += 1;
+      left++
     }
   }
 
-  return [];
+  return []
 }
 
 // twoNumberSum2 is O(nLog(n)) T | O(1) S
