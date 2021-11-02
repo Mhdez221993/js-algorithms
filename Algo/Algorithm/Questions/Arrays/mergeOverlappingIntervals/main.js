317a647400c67376336201cb0656ed59bc8b17bd
Moises <mhdezcoronado@gmail.com>
2021-11-02 02:15:15 +0100

    Update mergeOverlappingIntervals

--- Algo/Algorithm/Questions/Arrays/mergeOverlappingIntervals/main.js
+++ Algo/Algorithm/Questions/Arrays/mergeOverlappingIntervals/main.js
@@ -1,22 +1,28 @@
 function mergeOverlappingIntervals(array) {
   array = array.sort(([a],[b]) => a - b)
   let merge = [];
-  let curr = undefined;
+  let currFirst = undefined;
+  let currSecond = undefined;
   let [lastFirst, lastSecond] = array[array.length -1]
   for(let i = 0; i < array.length - 1; i++) {
     let [first, second] = array[i];
     let [firstNext, secondNext] = array[i+1];
 
-    first = curr ? curr : first
+    first = currFirst ? currFirst : first
+    second = currSecond ? currSecond : second
     if (second < firstNext) {
       merge.push([first, second])
-      curr = undefined
+      currFirst = undefined
+      currSecond = undefined
+    } else if (second > secondNext) {
+      currSecond = second
     } else {
-      curr = first
+      currFirst = first
     }
   }
 
-  lastFirst = curr ? curr : lastFirst
+  lastFirst = currFirst ? currFirst : lastFirst
+  // lastSecond = currSecond ? currSecond : lastFirst
   merge.push([lastFirst, lastSecond])
 
 
@@ -24,13 +30,18 @@ function mergeOverlappingIntervals(array) {
 }
 
 let array = [
-  [100, 105],
-  [1, 104]
+  [1, 2],
+  [3, 5],
+  [4, 7],
+  [6, 8],
+  [9, 10]
 ]
 
 let result = [
   [1, 2],
-  [3, 8],
+  [3, 5],
+  [4, 7],
+  [6, 8],
   [9, 10]
 ]
 
