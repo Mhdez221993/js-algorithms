aaf259a6a54f849d8dbe970a4258c45224567cdc
Moises <mhdezcoronado@gmail.com>
2021-11-02 15:00:03 +0100

    Add test for mergeOverlappingIntervals

--- Algo/Algorithm/Questions/Arrays/mergeOverlappingIntervals/main.test.js
+++ Algo/Algorithm/Questions/Arrays/mergeOverlappingIntervals/main.test.js
@@ -3,97 +3,223 @@ const mergeOverlappingIntervals = require('./main');
 describe('mergeOverlappingIntervals', () => {
   it('should return result', () => {
     const test = {
-      "array": [2, 1, 5, 2, 3, 3, 4]
+      "intervals": [
+        [1, 2],
+        [3, 5],
+        [4, 7],
+        [6, 8],
+        [9, 10]
+      ]
     }
-    let result = 2
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [1, 2],
+      [3, 8],
+      [9, 10]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array": [2, 1, 5, 3, 3, 2, 4]
+      "intervals": [
+        [1, 3],
+        [2, 8],
+        [9, 10]
+      ]
     };
-    let result = 3
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [1, 8],
+      [9, 10]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array": [1, 1, 2, 3, 3, 2, 2]
+      "intervals": [
+        [1, 10],
+        [10, 20],
+        [20, 30],
+        [30, 40],
+        [40, 50],
+        [50, 60],
+        [60, 70],
+        [70, 80],
+        [80, 90],
+        [90, 100]
+      ]
     };
-    let result = 1
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [1, 100]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array": [3, 1, 3, 1, 1, 4, 4]
+      "intervals": [
+        [1, 10],
+        [11, 20],
+        [21, 30],
+        [31, 40],
+        [41, 50],
+        [51, 60],
+        [61, 70],
+        [71, 80],
+        [81, 90],
+        [91, 100]
+      ]
     }
-    let result = 3
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [1, 10],
+      [11, 20],
+      [21, 30],
+      [31, 40],
+      [41, 50],
+      [51, 60],
+      [61, 70],
+      [71, 80],
+      [81, 90],
+      [91, 100]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array": []
+      "intervals": [
+        [100, 105],
+        [1, 104]
+      ]
     };
-    let result = -1
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [1, 105]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array":[1]
+      "intervals": [
+        [89, 90],
+        [-10, 20],
+        [-50, 0],
+        [70, 90],
+        [90, 91],
+        [90, 95]
+      ]
     };
-    let result = -1
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [-50, 20],
+      [70, 95]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array": [1, 1]
+      "intervals": [
+        [-5, -4],
+        [-4, -3],
+        [-3, -2],
+        [-2, -1],
+        [-1, 0]
+      ]
     };
-    let result = 1
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [-5, 0]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10]
+      "intervals": [
+        [43, 49],
+        [9, 12],
+        [12, 54],
+        [45, 90],
+        [91, 93]
+      ]
     };
-    let result = 10
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [9, 90],
+      [91, 93]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array": [2, 1, 1]
+      "intervals": [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0]
+      ]
     };
-    let result = 1
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [0, 0]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array": [2, 2, 2, 2, 2, 2, 2, 2, 2]
+      "intervals": [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 1]
+      ]
     };
-    let result = 2
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [0, 1]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array": [6, 15, 7, 10, 9, 14, 10, 1, 10, 1, 2, 11, 1, 6, 8]
+      "intervals": [
+        [1, 22],
+        [-20, 30]
+      ]
     };
-    let result = 10
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [-20, 30]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "array": [13, 4, 10, 10, 8, 13, 13, 7, 11, 6, 3, 2, 11]
+      "intervals": [
+        [20, 21],
+        [22, 23],
+        [0, 1],
+        [3, 4],
+        [23, 24],
+        [25, 27],
+        [5, 6],
+        [7, 19]
+      ]
     };
-    let result = 10
-    expect(mergeOverlappingIntervals(test.array)).toStrictEqual(result);
+    let result = [
+      [0, 1],
+      [3, 4],
+      [5, 6],
+      [7, 19],
+      [20, 21],
+      [22, 24],
+      [25, 27]
+    ]
+    expect(mergeOverlappingIntervals(test.intervals)).toStrictEqual(result);
   });
 });
