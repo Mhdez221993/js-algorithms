becc9df9aad7aba16a528688db2b4abb740b5181
Moises <mhdezcoronado@gmail.com>
2021-11-03 01:26:18 +0100

    Optimize fourNumberSum solution

--- Algo/Algorithm/Questions/Arrays/fourNumberSum/main.js
+++ Algo/Algorithm/Questions/Arrays/fourNumberSum/main.js
@@ -1,27 +1,23 @@
+// O(n^2) time and in the worst case O(n^3) time | O(n^2) space
 function fourNumberSum(array, targetSum) {
   let quadruplets = []
   let hash = {}
 
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 1; i < array.length - 1; i++) {
     let current = array[i]
 
     for (let j = i + 1; j < array.length; j++) {
       let key = targetSum - (current + array[j])
-      console.log(key, current, array[j]);
       let pairs = hash[key]
       if (pairs) {
-        pairs.map(v => {
-          let newValue = [...v, current, array[j]]
-          quadruplets.push(newValue)
-        })
+        pairs.map(v => quadruplets.push([ ...v, current, array[j] ]) )
       }
     }
 
     for (let j = 0; j < i; j++) {
       let key = current + array[j]
       if (!hash[key]) {
-        hash[key] = []
-        hash[key].push([array[j], current])
+        hash[key] = [[array[j], current]]
 
       } else {
         hash[key].push([current, array[j]])
