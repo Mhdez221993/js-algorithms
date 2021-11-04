5f16dfcae5e81ae8d67f69a1f586baa6882ba366
Moises <mhdezcoronado@gmail.com>
2021-11-04 18:23:35 +0100

    Code solution for subarraySort challenge

--- Algo/Algorithm/Questions/Arrays/subarraySort/main.js
+++ Algo/Algorithm/Questions/Arrays/subarraySort/main.js
@@ -1,7 +1,36 @@
 function subarraySort(array) {
+  let lowest = Infinity
+  let highest = -Infinity
+  for(let i = 0; i < array.length; i++){
+    let breakingPoint = array[i] > array[i + 1]
+
+    if (breakingPoint) {
+      highest = array[i] > highest ? array[i] : highest
+      lowest = array[i + 1] < lowest ? array[i + 1] : lowest
+    }
+  }
+
+  if (lowest == Infinity) {
+    return [-1, -1]
+  }
+
+  let foundI = true
+  let curr = highest
+  array.forEach((element, index) => {
+    if (element > lowest && foundI){
+      lowest = index
+      foundI = false
+    }
+
+    if (element < curr) {
+      highest = index
+    }
+  });
+
+  return [lowest, highest]
 
 }
 
-let array = [1, 2, 4, 7, 10, 11, 7, 12, 13, 14, 16, 18, 19]
+let array = [1, 2, 3, 4, 5, 6, 18, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19]
 console.log(subarraySort(array));
 // module.exports = subarraySort;
