55b86c6fa1ad0267c0e6405a4b48dcc8ed0faebc
Moises <mhdezcoronado@gmail.com>
2021-11-06 18:11:54 +0100

    Optimize largestRange when the imput is length 1

--- Algo/Algorithm/Questions/Arrays/largestRange/main.js
+++ Algo/Algorithm/Questions/Arrays/largestRange/main.js
@@ -1,6 +1,6 @@
 function largestRange(array) {
-  let first = 0
-  let second = 0
+  let first = array[0]
+  let second = array[0]
   let hash = {}
   array.forEach(element => {
     hash[element] = true
@@ -8,22 +8,27 @@ function largestRange(array) {
 
   for (let i = 0; i < array.length; i++) {
     let j = array[i]
+    hash[j] = false
     while(hash[j - 1]){
+      hash[j - 1] = false
       j--
     }
 
     let k = array[i]
+    hash[k] = false
     while (hash[k + 1]) {
+      hash[k + 1] = false
       k++
     }
 
     if (k - j > second - first) {
       first = j
       second = k
+
     }
   }
   return [first, second]
 }
 
-let array = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6];
+let array = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6]
 console.log(largestRange(array));
