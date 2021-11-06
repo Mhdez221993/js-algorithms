527a6fcb2fee372e7fdcc624447ec24016164c33
Moises <mhdezcoronado@gmail.com>
2021-11-07 00:44:49 +0100

    Add test for largestRange

--- Algo/Algorithm/Questions/Arrays/largestRange/main.test.js
+++ Algo/Algorithm/Questions/Arrays/largestRange/main.test.js
@@ -49,51 +49,59 @@ describe('largestRange', () => {
     expect(largestRange(test.array)).toStrictEqual(result);
   });
 
-  // it('should return result', () => {
-  //   const test = {
-  //     "array": [5, 4, 3, 2, 1, 2, 10, 12]
-  //   };
-  //   let result = 0
-  //   expect(largestRange(test.array)).toStrictEqual(result);
-  // });
+  it('should return result', () => {
+    const test = {
+      "array": [19, -1, 18, 17, 2, 10, 3, 12, 5, 16, 4, 11, 8, 7, 6, 15, 12, 12, 2, 1, 6, 13, 14]
+    }
+    let result = [10, 19]
+    expect(largestRange(test.array)).toStrictEqual(result);
+  });
 
-  // it('should return result', () => {
-  //   const test = {
-  //     "array": [1, 2, 3, 4, 5, 6, 10, 100, 1000]
-  //   };
-  //   let result = 0
-  //   expect(largestRange(test.array)).toStrictEqual(result);
-  // });
+  it('should return result', () => {
+    const test = {
+      "array": [0, 9, 19, -1, 18, 17, 2, 10, 3, 12, 5, 16, 4, 11, 8, 7, 6, 15, 12, 12, 2, 1, 6, 13, 14]
+    }
+    let result = [-1, 19]
+    expect(largestRange(test.array)).toStrictEqual(result);
+  });
 
-  // it('should return result', () => {
-  //   const test = {
-  //     "array": [1, 2, 3, 3, 2, 1]
-  //   };
-  //   let result = 0
-  //   expect(largestRange(test.array)).toStrictEqual(result);
-  // });
+  it('should return result', () => {
+    const test ={
+      "array": [0, -5, 9, 19, -1, 18, 17, 2, -4, -3, 10, 3, 12, 5, 16, 4, 11, 7, -6, -7, 6, 15, 12, 12, 2, 1, 6, 13, 14, -2]
+    }
+    let result = [-7, 7]
+    expect(largestRange(test.array)).toStrictEqual(result);
+  });
 
-  // it('should return result', () => {
-  //   const test = {
-  //     "array": [1, 1, 3, 2, 1]
-  //   };
-  //   let result = 4
-  //   expect(largestRange(test.array)).toStrictEqual(result);
-  // });
+  it('should return result', () => {
+    const test ={
+      "array": [-7, -7, -7, -7, 8, -8, 0, 9, 19, -1, -3, 18, 17, 2, 10, 3, 12, 5, 16, 4, 11, -6, 8, 7, 6, 15, 12, 12, -5, 2, 1, 6, 13, 14, -4, -2]
+    }
+    let result = [-8, 19]
+    expect(largestRange(test.array)).toStrictEqual(result);
+  });
 
-  // it('should return result', () => {
-  //   const test = {
-  //     "array": [1, 2, 3, 2, 1, 1]
-  //   };
-  //   let result = 5
-  //   expect(largestRange(test.array)).toStrictEqual(result);
-  // });
+  it('should return result', () => {
+    const test = {
+      "array": [1, 1, 1, 3, 4]
+    }
+    let result = [3, 4]
+    expect(largestRange(test.array)).toStrictEqual(result);
+  });
 
-  // it('should return result', () => {
-  //   const test = {
-  //     "array": [1, 1, 1, 2, 3, 10, 12, -3, -3, 2, 3, 45, 800, 99, 98, 0, -1, -1, 2, 3, 4, 5, 0, -1, -1]
-  //   };
-  //   let result = 9
-  //   expect(largestRange(test.array)).toStrictEqual(result);
-  // });
+  it('should return result', () => {
+    const test = {
+      "array": [1, 1, 1, 3, 4]
+    }
+    let result = [3, 4]
+    expect(largestRange(test.array)).toStrictEqual(result);
+  });
+
+  it('should return result', () => {
+    const test = {
+      "array": [-1, 0, 1]
+    }
+    let result = [-1, 1]
+    expect(largestRange(test.array)).toStrictEqual(result);
+  });
 });
