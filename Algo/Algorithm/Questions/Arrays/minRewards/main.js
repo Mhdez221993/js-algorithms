13942f9599f6ade16871be91566d93f9f653af4e
Moises <mhdezcoronado@gmail.com>
2021-11-07 17:54:03 +0100

    Optimize minRewards

--- Algo/Algorithm/Questions/Arrays/minRewards/main.js
+++ Algo/Algorithm/Questions/Arrays/minRewards/main.js
@@ -2,28 +2,30 @@ function minRewards(scores) {
   let biggest = 1
   let smallest = 1
   let reward = 0
+  let accending = false
 
   for (let i = 0; i < scores.length; i++) {
     reward++
 
     if (scores[i] < scores[i - 1]) {
+      if (accending) {
 
-      if(i === scores.length - 1){
-        return reward
-      } else {
-        reward += smallest
-        smallest++
-        biggest = 1
+        accending = false
+        continue
       }
+      reward += smallest
+      smallest++
+      biggest = 1
 
     } else if(scores[i] > scores[i - 1]) {
       reward += biggest
       biggest++
       smallest = 1
+      accending = true
     }
   }
   return reward
 }
 
-let scores = [8, 4, 2, 1, 3, 6, 7, 9, 5]
+let scores = [0, 4, 2, 1, 3]
 console.log(minRewards(scores));
