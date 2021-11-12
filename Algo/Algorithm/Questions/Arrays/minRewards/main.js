3edeeaf809c69fd1c8951c82a3c96fcd39ac3fc8
Moises <mhdezcoronado@gmail.com>
2021-11-12 02:02:37 +0100

    Optimize minRewards

--- Algo/Algorithm/Questions/Arrays/minRewards/main.js
+++ Algo/Algorithm/Questions/Arrays/minRewards/main.js
@@ -3,19 +3,19 @@ function minRewards(scores) {
    let rewards = new Array(scores.length).fill(1)
 
    for(let i = 1; i < scores.length; i++) {
-      let j = i - 1
       if (scores[i] > scores[i - 1]) {
          rewards[i] = rewards[i - 1] + 1
-
-      } else {
-         while (j >= 0 && scores[j] > scores[j + 1]) {
-            rewards[j] = Math.max(rewards[j], rewards[j + 1] + 1)
-            j--
-         }
       }
    }
+
+   for(let i = scores.length - 2; i >= 0; i--) {
+      if (scores[i] > scores[i + 1]) {
+         rewards[i] = Math.max(rewards[i], rewards[i + 1] + 1)
+      }
+   }
+
    return rewards.reduce((acc, curr) => acc + curr)
 }
 
-let scores = [8, 4, 2, 1, 3, 6, 7, 9, 5]
-console.log(minRewards(scores));
+
+module.exports = minRewards
