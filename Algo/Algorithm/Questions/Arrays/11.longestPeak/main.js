9b33f28fef9c46519d42eac2d3b6754638d94057
Moises <mhdezcoronado@gmail.com>
2021-11-12 11:20:03 +0100

    Optimize longestPeak

---
+++ Algo/Algorithm/Questions/Arrays/11.longestPeak/main.js
@@ -0,0 +1,31 @@
+function longestPeak(array) {
+  let longestPeak = 0
+  let i = 1
+
+  while(i < array.length - 1){
+    let peak = array[i] > array[i - 1] && array[i] > array[i + 1]
+
+    if (!peak){
+      i++
+      continue
+    }
+
+    let leftIndx = i - 2
+    while(leftIndx >= 0 && array[leftIndx] < array[leftIndx + 1]){
+      leftIndx--
+    }
+
+    let rightIndx = i + 2
+    while(rightIndx < array.length && array[rightIndx] < array[rightIndx - 1]){
+      rightIndx++
+    }
+
+    let currentPeak = rightIndx - leftIndx - 1
+    longestPeak = Math.max(currentPeak, longestPeak)
+    i = rightIndx
+  }
+
+  return longestPeak
+}
+
+module.exports = longestPeak;
