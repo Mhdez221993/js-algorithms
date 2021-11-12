f447b4f75e399511e83817d9482400430aeaef85
Moises <mhdezcoronado@gmail.com>
2021-11-12 11:20:40 +0100

    Optimize arrayOfProducts

---
+++ Algo/Algorithm/Questions/Arrays/12.arrayOfProducts/main.js
@@ -0,0 +1,19 @@
+function arrayOfProducts(array) {
+  let products = new Array(array.length).fill(1)
+
+  let leftCurrentProduct = 1
+  for(let i = 0; i < array.length; i++){
+    products[i] *= leftCurrentProduct
+    leftCurrentProduct *= array[i]
+  }
+
+  let rigthCurrentProduct = 1
+  for(let i = array.length - 1; i >= 0; i--){
+    products[i] *= rigthCurrentProduct
+    rigthCurrentProduct *= array[i]
+  }
+
+  return products
+}
+
+module.exports = arrayOfProducts;
