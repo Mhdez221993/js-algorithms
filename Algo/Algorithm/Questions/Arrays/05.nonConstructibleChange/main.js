172b6d256db2af23e55f744617101b36a02ece0d
Moises <mhdezcoronado@gmail.com>
2021-11-20 16:44:31 +0100

    Replace map to be a forEach

--- Algo/Algorithm/Questions/Arrays/05.nonConstructibleChange/main.js
+++ Algo/Algorithm/Questions/Arrays/05.nonConstructibleChange/main.js
@@ -2,8 +2,8 @@ function nonConstructibleChange(coins) {
   coins.sort((a, b) => a - b);
 
   let currentChange = 0;
-  coins.map(v => {
-    if (v > currentChange + 1) return currentChange + 1;
+  coins.forEach(v => {
+    if (v > currentChange + 1) return;
     currentChange += v;
   });
 
