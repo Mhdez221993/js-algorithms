b59b7941cab7964257601f79c8f50d04a7a5980d
Moises <mhdezcoronado@gmail.com>
2021-11-25 18:24:43 +0100

    Fix linters error on test

--- Algo/Algorithm/Questions/Arrays/20.apartmentHunting/main.test.js
+++ Algo/Algorithm/Questions/Arrays/20.apartmentHunting/main.test.js
@@ -3,489 +3,489 @@ const apartmentHunting = require('./main');
 describe('apartmentHunting', () => {
   it('should return result', () => {
     const test = {
-      "blocks": [
+      blocks: [
         {
-          "gym": false,
-          "school": true,
-          "store": false
+          gym: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": true,
-          "school": false,
-          "store": false
+          gym: true,
+          school: false,
+          store: false,
         },
         {
-          "gym": true,
-          "school": true,
-          "store": false
+          gym: true,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "school": true,
-          "store": false
+          gym: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "school": true,
-          "store": true
-        }
+          gym: false,
+          school: true,
+          store: true,
+        },
       ],
-      "reqs": ["gym", "school", "store"]
-    }
-    const result = 3
+      reqs: ['gym', 'school', 'store'],
+    };
+    const result = 3;
     expect(apartmentHunting(test.blocks, test.reqs)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "blocks": [
+      blocks: [
         {
-          "gym": false,
-          "office": true,
-          "school": true,
-          "store": false
+          gym: false,
+          office: true,
+          school: true,
+          store: false,
         },
         {
-          "gym": true,
-          "office": false,
-          "school": false,
-          "store": false
+          gym: true,
+          office: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": true,
-          "office": false,
-          "school": true,
-          "store": false
+          gym: true,
+          office: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "school": true,
-          "store": false
+          gym: false,
+          office: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "school": true,
-          "store": true
-        }
+          gym: false,
+          office: false,
+          school: true,
+          store: true,
+        },
       ],
-      "reqs": ["gym", "office", "school", "store"]
-    }
+      reqs: ['gym', 'office', 'school', 'store'],
+    };
     const result = 2;
     expect(apartmentHunting(test.blocks, test.reqs)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "blocks": [
+      blocks: [
         {
-          "gym": false,
-          "office": true,
-          "school": true,
-          "store": false
+          gym: false,
+          office: true,
+          school: true,
+          store: false,
         },
         {
-          "gym": true,
-          "office": false,
-          "school": false,
-          "store": false
+          gym: true,
+          office: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": true,
-          "office": false,
-          "school": true,
-          "store": false
+          gym: true,
+          office: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "school": true,
-          "store": false
+          gym: false,
+          office: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "school": true,
-          "store": false
+          gym: false,
+          office: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "school": true,
-          "store": true
-        }
+          gym: false,
+          office: false,
+          school: true,
+          store: true,
+        },
       ],
-      "reqs": ["gym", "office", "school", "store"]
-    }
+      reqs: ['gym', 'office', 'school', 'store'],
+    };
     const result = 2;
     expect(apartmentHunting(test.blocks, test.reqs)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "blocks": [
+      blocks: [
         {
-          "foo": true,
-          "gym": false,
-          "kappa": false,
-          "office": true,
-          "school": true,
-          "store": false
+          foo: true,
+          gym: false,
+          kappa: false,
+          office: true,
+          school: true,
+          store: false,
         },
         {
-          "foo": true,
-          "gym": true,
-          "kappa": false,
-          "office": false,
-          "school": false,
-          "store": false
+          foo: true,
+          gym: true,
+          kappa: false,
+          office: false,
+          school: false,
+          store: false,
         },
         {
-          "foo": true,
-          "gym": true,
-          "kappa": false,
-          "office": false,
-          "school": true,
-          "store": false
+          foo: true,
+          gym: true,
+          kappa: false,
+          office: false,
+          school: true,
+          store: false,
         },
         {
-          "foo": true,
-          "gym": false,
-          "kappa": false,
-          "office": false,
-          "school": true,
-          "store": false
+          foo: true,
+          gym: false,
+          kappa: false,
+          office: false,
+          school: true,
+          store: false,
         },
         {
-          "foo": true,
-          "gym": true,
-          "kappa": false,
-          "office": false,
-          "school": true,
-          "store": false
+          foo: true,
+          gym: true,
+          kappa: false,
+          office: false,
+          school: true,
+          store: false,
         },
         {
-          "foo": true,
-          "gym": false,
-          "kappa": false,
-          "office": false,
-          "school": true,
-          "store": true
-        }
+          foo: true,
+          gym: false,
+          kappa: false,
+          office: false,
+          school: true,
+          store: true,
+        },
       ],
-      "reqs": ["gym", "school", "store"]
-    }
+      reqs: ['gym', 'school', 'store'],
+    };
     const result = 4;
     expect(apartmentHunting(test.blocks, test.reqs)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "blocks": [
+      blocks: [
         {
-          "gym": true,
-          "school": true,
-          "store": false
+          gym: true,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "school": false,
-          "store": false
+          gym: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "school": true,
-          "store": false
+          gym: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "school": false,
-          "store": false
+          gym: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "school": false,
-          "store": true
+          gym: false,
+          school: false,
+          store: true,
         },
         {
-          "gym": true,
-          "school": false,
-          "store": false
+          gym: true,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "school": false,
-          "store": false
+          gym: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "school": false,
-          "store": false
+          gym: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "school": false,
-          "store": false
+          gym: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "school": true,
-          "store": false
-        }
+          gym: false,
+          school: true,
+          store: false,
+        },
       ],
-      "reqs": ["gym", "school", "store"]
-    }
-    const result = 2
+      reqs: ['gym', 'school', 'store'],
+    };
+    const result = 2;
     expect(apartmentHunting(test.blocks, test.reqs)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "blocks": [
+      blocks: [
         {
-          "gym": true,
-          "pool": false,
-          "school": true,
-          "store": false
+          gym: true,
+          pool: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "pool": false,
-          "school": true,
-          "store": false
+          gym: false,
+          pool: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "pool": false,
-          "school": false,
-          "store": true
+          gym: false,
+          pool: false,
+          school: false,
+          store: true,
         },
         {
-          "gym": true,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: true,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "pool": false,
-          "school": true,
-          "store": false
+          gym: false,
+          pool: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "pool": true,
-          "school": false,
-          "store": false
-        }
+          gym: false,
+          pool: true,
+          school: false,
+          store: false,
+        },
       ],
-      "reqs": ["gym", "pool", "school", "store"]
-    }
+      reqs: ['gym', 'pool', 'school', 'store'],
+    };
     const result = 7;
     expect(apartmentHunting(test.blocks, test.reqs)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "blocks": [
+      blocks: [
         {
-          "gym": true,
-          "office": false,
-          "pool": false,
-          "school": true,
-          "store": false
+          gym: true,
+          office: false,
+          pool: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          office: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": true,
-          "pool": false,
-          "school": true,
-          "store": false
+          gym: false,
+          office: true,
+          pool: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": true,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          office: true,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": false,
-          "school": false,
-          "store": true
+          gym: false,
+          office: false,
+          pool: false,
+          school: false,
+          store: true,
         },
         {
-          "gym": true,
-          "office": true,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: true,
+          office: true,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": true,
-          "school": false,
-          "store": false
+          gym: false,
+          office: false,
+          pool: true,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          office: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          office: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": false,
-          "school": true,
-          "store": false
+          gym: false,
+          office: false,
+          pool: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": true,
-          "school": false,
-          "store": false
-        }
+          gym: false,
+          office: false,
+          pool: true,
+          school: false,
+          store: false,
+        },
       ],
-      "reqs": ["gym", "pool", "school", "store"]
-    }
-    const result = 4
+      reqs: ['gym', 'pool', 'school', 'store'],
+    };
+    const result = 4;
     expect(apartmentHunting(test.blocks, test.reqs)).toStrictEqual(result);
   });
 
   it('should return result', () => {
     const test = {
-      "blocks": [
+      blocks: [
         {
-          "gym": true,
-          "office": false,
-          "pool": false,
-          "school": true,
-          "store": false
+          gym: true,
+          office: false,
+          pool: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          office: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": true,
-          "pool": false,
-          "school": true,
-          "store": false
+          gym: false,
+          office: true,
+          pool: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": true,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          office: true,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": false,
-          "school": false,
-          "store": true
+          gym: false,
+          office: false,
+          pool: false,
+          school: false,
+          store: true,
         },
         {
-          "gym": true,
-          "office": true,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: true,
+          office: true,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": true,
-          "school": false,
-          "store": false
+          gym: false,
+          office: false,
+          pool: true,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          office: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": false,
-          "school": false,
-          "store": false
+          gym: false,
+          office: false,
+          pool: false,
+          school: false,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": false,
-          "school": true,
-          "store": false
+          gym: false,
+          office: false,
+          pool: false,
+          school: true,
+          store: false,
         },
         {
-          "gym": false,
-          "office": false,
-          "pool": true,
-          "school": false,
-          "store": false
-        }
+          gym: false,
+          office: false,
+          pool: true,
+          school: false,
+          store: false,
+        },
       ],
-      "reqs": ["gym", "pool", "school", "store"]
-    }
+      reqs: ['gym', 'pool', 'school', 'store'],
+    };
     const result = 4;
     expect(apartmentHunting(test.blocks, test.reqs)).toStrictEqual(result);
   });
