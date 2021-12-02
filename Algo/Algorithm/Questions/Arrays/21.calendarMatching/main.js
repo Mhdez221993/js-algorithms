560dc020af3d066b84ba3d00cb75043902de4598
Moises <mhdezcoronado@gmail.com>
2021-12-02 18:09:37 +0100

    Remove unnessessary code

--- Algo/Algorithm/Questions/Arrays/21.calendarMatching/main.js
+++ Algo/Algorithm/Questions/Arrays/21.calendarMatching/main.js
@@ -32,11 +32,3 @@ function calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meet
   }
 
 }
-
-let calendar1 = [['9:00', '10:30'], ['12:00', '13:00'], ['16:00', '18:00']]
-let dailyBounds1 = ['9:00', '20:00']
-let calendar2 = [['10:00', '11:30'], ['12:30', '14:30'], ['14:30', '15:00'], ['16:00', '17:00']]
-let dailyBounds2 = ['10:00', '18:30']
-let meetingDuration = 30
-
-console.log(calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meetingDuration));
