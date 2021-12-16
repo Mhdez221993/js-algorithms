7837cf68704c73fe02e34d6c283b1ae21990fab1
Moises <mhdezcoronado@gmail.com>
2021-12-17 00:48:46 +0100

    Solve Toeplitz Matrix algo

---
+++ pramp/Algorithms/01.toeplitz_matrix/main.js
@@ -0,0 +1,21 @@
+function isToeplitz(arr) {
+  if (arr.length === 1) {
+    return true
+  }
+
+  for (let i = 1; i < arr.length; i += 1) {
+    if (arr[i].length === 1) {
+      return true
+    }
+
+    for (let j = 1; j < arr[i].length; j += 1) {
+
+      if (arr[i][j] !== arr[i - 1][j - 1]) {
+        return false
+      }
+    }
+  }
+
+  return true
+}
+
