3f5aaa673df342ce51b90916b83f83c3c98c7557
Moises <mhdezcoronado@gmail.com>
2021-12-20 00:58:16 +0100

    Export calendarMatching function

--- Algo/Algorithm/Questions/Arrays/21.calendarMatching/main.js
+++ Algo/Algorithm/Questions/Arrays/21.calendarMatching/main.js
@@ -92,10 +92,4 @@ function calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meet
   return getMatchingAvailabilities(flattedCalendar, meetingDuration)
 }
 
-let calendar1 = [['9:00', '10:30'], ['12:00', '13:00'], ['16:00', '18:00']]
-let dailyBounds1 = ['9:00', '20:00']
-let calendar2 = [['10:00', '11:30'], ['12:30', '14:30'], ['14:30', '15:00'], ['16:00', '17:00']]
-let dailyBounds2 = ['10:00', '18:30']
-let meetingDuration = 30
-
-console.log(calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meetingDuration));
+module.exports = calendarMatching
