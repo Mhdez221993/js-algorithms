4f6050b8ce7dc88337a73de7c72a0443ec1d1686
Moises <mhdezcoronado@gmail.com>
2021-12-20 22:44:56 +0100

    Add Js solutio for minimumAreaRectangle

---
+++ Algo/Algorithm/Questions/Arrays/23.minimumAreaRectangle/main.js
@@ -0,0 +1,59 @@
+function initilizeColumns(points) {
+  let columns = {}
+  for (let i = 0; i < points.length; i += 1) {
+    let [x, y] = points[i]
+    if (!columns[x]) {
+      columns[x] = []
+    }
+    columns[x] = [...columns[x], y]
+  }
+
+  return columns
+}
+
+function minimumAreaRectangle(points) {
+  const columns = initilizeColumns(points)
+  let minumumAreaFound = Infinity
+  const edgesParallelToYAxis = {}
+
+  const sortedColumns = Object.keys(columns)
+    .map(col => parseInt(col))
+    .sort((a, b) => a - b)
+
+  for (const x of sortedColumns) {
+    const yValuesInCurrentColumn = columns[x].sort((a, b) => a - b)
+
+    for (let currIdx = 0; currIdx < yValuesInCurrentColumn.length; currIdx += 1) {
+      const y2 = yValuesInCurrentColumn[currIdx]
+      for (let prevIdx = 0; prevIdx < currIdx; prevIdx++) {
+        const y1 = yValuesInCurrentColumn[prevIdx]
+        const pointString = y1.toString() + ':' + y2.toString()
+
+        if (pointString in edgesParallelToYAxis) {
+          const currArea = (x - edgesParallelToYAxis[pointString]) * (y2 - y1)
+          minumumAreaFound = Math.min(minumumAreaFound, currArea)
+        }
+
+        edgesParallelToYAxis[pointString] = x
+      }
+    }
+  }
+
+  return minumumAreaFound !== Infinity ? minumumAreaFound : 0
+}
+
+let test = {
+  points: [
+    [1, 5],
+    [5, 1],
+    [4, 2],
+    [2, 4],
+    [2, 2],
+    [1, 2],
+    [4, 5],
+    [2, 5],
+    [-1, -2]
+  ]
+}
+
+console.log(minimumAreaRectangle(test.points));
