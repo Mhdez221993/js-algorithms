635308df9cfdb1437384f349774550ab4b5d308a
Moises <mhdezcoronado@gmail.com>
2021-12-20 01:11:29 +0100

    Test calendarMatching

--- Algo/Algorithm/Questions/Arrays/21.calendarMatching/main.test.js
+++ Algo/Algorithm/Questions/Arrays/21.calendarMatching/main.test.js
@@ -1,5 +1,37 @@
-function calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meetingDuration) {
-  // Write your code here.
-}
+const calendarMatching = require('./main');
 
-module.exports = calendarMatching;
+describe('calendarMatching', () => {
+  it('should return result', () => {
+    const test = {
+      calendar1: [
+        ['9:00', '10:30'],
+        ['12:00', '13:00'],
+        ['16:00', '18:00'],
+      ],
+      calendar2: [
+        ['10:00', '11:30'],
+        ['12:30', '14:30'],
+        ['14:30', '15:00'],
+        ['16:00', '17:00'],
+      ],
+      dailyBounds1: ['9:00', '20:00'],
+      dailyBounds2: ['10:00', '18:30'],
+      meetingDuration: 30,
+    };
+
+    const result = [
+      ['11:30', '12:00'],
+      ['15:00', '16:00'],
+      ['18:00', '18:30'],
+    ];
+    expect(
+      calendarMatching(
+        test.calendar1,
+        test.dailyBounds1,
+        test.calendar2,
+        test.dailyBounds2,
+        test.meetingDuration,
+      ),
+    ).toStrictEqual(result);
+  });
+});
