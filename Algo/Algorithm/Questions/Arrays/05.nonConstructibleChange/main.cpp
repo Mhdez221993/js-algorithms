bbf825c87b3b0e6ae13d3ef3976b96df03d6d7d6
Moises <mhdezcoronado@gmail.com>
2021-12-22 01:20:49 +0100

    Add c++ solution for nonConstructibleChangle algo

---
+++ Algo/Algorithm/Questions/Arrays/05.nonConstructibleChange/main.cpp
@@ -0,0 +1,27 @@
+// O(nlogn) time | O(1) space - where n is the number of coins
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int nonConstructibleChange(vector<int> coins)
+{
+  sort(coins.begin(), coins.end());
+  int minimumChangeICannotCreate = 1;
+  for(int coin :coins)
+  {
+    if(coin > minimumChangeICannotCreate)
+    {
+      return minimumChangeICannotCreate;
+    }
+    minimumChangeICannotCreate += coin;
+  }
+
+  return minimumChangeICannotCreate;
+}
+
+int main()
+{
+  vector<int> coins = {5, 7, 1, 1, 2, 3, 22};
+  cout << nonConstructibleChange(coins) << endl;
+}
