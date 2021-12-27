9d5885b45db929fe24408575c387946ed3c761b1
Moises <mhdezcoronado@gmail.com>
2021-12-27 17:29:12 +0100

    Optimize longestPeak solution

--- Algo/Algorithm/Questions/Arrays/11.longestPeak/main.cpp
+++ Algo/Algorithm/Questions/Arrays/11.longestPeak/main.cpp
@@ -6,30 +6,30 @@ using namespace std;
 int longestPeak(vector<int> array)
 {
   int peak = 0;
-  int len = array.size() - 1;
-  for (int i = 1; i < len; i++)
+  int i = 1;
+  while (i < int(array.size() - 1))
   {
     bool isPeak = (array[i] > array[i - 1]) && (array[i] > array[i + 1]);
-    if (isPeak)
+    if (!isPeak)
     {
-      int left = i - 1;
-      int currentPeak = 1;
-      while (left >= 0 && array[left] < array[left + 1])
-      {
-        currentPeak++;
-        left--;
-      }
-      int right = i + 1;
-      while (right < array.size() && array[right] < array[right - 1])
-      {
-        currentPeak++;
-        right++;
-        i++;
-      }
-
-      if (currentPeak > peak)
-        peak = currentPeak;
+      i++;
+      continue;
     }
+
+    int left = i - 2;
+    while (left >= 0 && array[left] < array[left + 1])
+    {
+      left--;
+    }
+    int right = i + 2;
+    while (right < array.size() && array[right] < array[right - 1])
+    {
+      right++;
+    }
+
+    int currentPeak = right - left - 1;
+    peak = max(peak, currentPeak);
+    i = right;
   }
 
   return peak;
@@ -38,7 +38,7 @@ int longestPeak(vector<int> array)
 int main()
 {
 
-  vector<int> array = {5, 4, 3, 2, 1, 2, 10, 12};
+  vector<int> array = {1, 2, 3, 3, 4, 0, 10, 6};
   cout << longestPeak(array) << endl;
   return 0;
 }
