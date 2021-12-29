6c7a59180aca2382dc3e35dab8a93a4b64975765
Moises <mhdezcoronado@gmail.com>
2021-12-29 14:18:03 +0100

    Add c++ solution for mergeOverlappingIntervals

---
+++ Algo/Algorithm/Questions/Arrays/14.mergeOverlappingIntervals/main.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool overlapping(vector<int> prevInterval, vector<int> currInterval)
+{
+  if (currInterval[0] <= prevInterval[1])
+  {
+    return true;
+  }
+
+  return false;
+}
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> intervals)
+{
+  vector<vector<int>> result;
+  sort(intervals.begin(), intervals.end());
+  vector<int> prevInterval = intervals[0];
+
+  for (int i = 1; i < intervals.size(); i++)
+  {
+    vector<int> currInterval = intervals[i];
+    if (overlapping(prevInterval, currInterval))
+    {
+      prevInterval[0] = min(prevInterval[0], currInterval[0]);
+      prevInterval[1] = max(prevInterval[1], currInterval[1]);
+    }
+    else
+    {
+      result.push_back(prevInterval);
+      prevInterval = currInterval;
+    }
+  }
+
+  result.push_back(prevInterval);
+  return result;
+}
+
+int main()
+{
+  vector<vector<int>> array = {
+      {1, 22},
+      {-20, 30}};
+  vector<vector<int>> result = mergeOverlappingIntervals(array);
+  for (vector<int> arr : result)
+  {
+    cout << arr[0] << " " << arr[1] << endl;
+  }
+  return 0;
+}
