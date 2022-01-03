2e5733394110849e0f575ea6b525d6926f733624
Moises <mhdezcoronado@gmail.com>
2022-01-03 13:30:08 +0100

    Add initial solution for sudokuSolve

--- Algo/Algorithm/Questions/Recursion/14.sudokuSolve/main.cpp
+++ Algo/Algorithm/Questions/Recursion/14.sudokuSolve/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-bool isTheCorrectNumber(int row, int col, int num, vector<vector<char>> &board)
+bool isTheCorrectNumber(int num, int row, int col, vector<vector<int>> &board)
 {
-  cout << endl;
   for (int i = 0; i < board.size(); i++)
   {
     if (board[row][i] == num)
@@ -17,54 +17,74 @@ bool isTheCorrectNumber(int row, int col, int num, vector<vector<char>> &board)
       return false;
   }
 
-  return true;
-}
-
-bool sudokuSolveII(vector<vector<char>> &board)
-{
-  for (int row = 0; row < board.size(); row++)
+  int subRow = floor(row / 3) * 3;
+  int subCol = floor(col / 3) * 3;
+  for (int i = subRow; i < (subRow + 3); i++)
   {
-    for (int col = 0; col < board[0].size(); col++)
+    for (int j = subCol; j < (subCol + 3); j++)
     {
-      if (board[row][col] == '.')
-      {
-        bool canFill = false;
-        for (int num = 1; num <= board.size(); num++)
-        {
-          char numToChar = num + '0';
-          if (isTheCorrectNumber(row, col, numToChar, board))
-            board[row][col] = numToChar;
-          for (vector<char> x : board)
-          {
-            cout << x[0] << " " << x[1] << " " << x[2] << endl;
-          }
-          // if (sudokuSolveII(board))
-          //   canFill = true;
-        }
-
-        // if (canFill == false)
-        // {
-        //   board[row][col] = '.';
-        //   return false;
-        // }
-      }
+      if (board[i][j] == num)
+        return false;
     }
   }
 
-  return false;
+  return true;
 }
 
-bool sudokuSolve(const vector<vector<char>> &board)
+bool solveParcialSudoku(int row, int col, vector<vector<int>> &board)
 {
-  vector<vector<char>> myBoard = board;
-  return sudokuSolveII(myBoard);
+  for (vector<int> x : board)
+  {
+    cout << x[0] << " " << x[1] << " " << x[2] << " " << x[3] << " " << x[4] << " " << x[5] << " " << x[6] << " " << x[7] << " " << x[8] << endl;
+  }
+  cout << endl;
+  int currentRow = row;
+  int currentCol = col;
+
+  if (currentCol == board[row].size())
+  {
+    currentRow += 1;
+    currentCol = 0;
+    if (currentRow == board.size())
+      return true;
+  }
+
+  if (board[currentRow][currentCol] == 0)
+  {
+    for (int num = 1; num <= 9; num++)
+    {
+      if (isTheCorrectNumber(num, row, col, board))
+      {
+        board[row][col] = num;
+        if (solveParcialSudoku(row, col + 1, board))
+          return true;
+      }
+    }
+
+    board[row][col] = 0;
+    return false;
+  }
+
+  return solveParcialSudoku(currentRow, currentCol + 1, board);
+}
+
+bool sudokuSolve(const vector<vector<int>> &board)
+{
+  vector<vector<int>> myBoard = board;
+  return solveParcialSudoku(0, 0, myBoard);
 }
 
 int main()
 {
-  vector<vector<char>> board = {
-      {'3', '.', '.'},
-      {'2', '3', '.'},
-      {'.', '1', '2'}};
+  vector<vector<int>> board = {
+      {7, 8, 0, 4, 0, 0, 1, 2, 0},
+      {6, 0, 0, 0, 7, 5, 0, 0, 9},
+      {0, 0, 0, 6, 0, 1, 0, 7, 8},
+      {0, 0, 7, 0, 4, 0, 2, 6, 0},
+      {0, 0, 1, 0, 5, 0, 9, 3, 0},
+      {9, 0, 4, 0, 6, 0, 0, 0, 5},
+      {0, 7, 0, 3, 0, 0, 0, 1, 2},
+      {1, 2, 0, 0, 0, 7, 4, 0, 0},
+      {0, 4, 9, 2, 0, 6, 0, 0, 7}};
   cout << sudokuSolve(board) << endl;
 }
