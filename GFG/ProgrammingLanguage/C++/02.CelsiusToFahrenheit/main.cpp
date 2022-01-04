2152b1ddd45173e10d6e5dd37212257b69d9e0fd
Moises <mhdezcoronado@gmail.com>
2022-01-04 18:53:20 +0100

    Convert Celsius to Fahrenheits

---
+++ GFG/ProgrammingLanguage/C++/02.CelsiusToFahrenheit/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Write a program to convert temperature given in Celsius(user input) to Fahrenheit.
+int main()
+{
+  int cel1 = 47;
+  int cel2 = 29;
+  cout << "47 deg Celsius in Fahrenheit: " << (9.0 / 5 * cel1 + 32) << endl;
+  cout << "100 deg Celsius in Fahrenheit: " << (9.0 / 5 * cel2 + 32) << endl;
+  return 0;
+}
