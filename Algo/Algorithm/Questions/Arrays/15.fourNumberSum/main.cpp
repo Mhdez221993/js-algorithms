ee3343974a3ca8a3e198f341be42adb48fbd4021
Moises <mhdezcoronado@gmail.com>
2022-01-04 15:40:03 +0100

    Remove unessessary code

--- Algo/Algorithm/Questions/Arrays/15.fourNumberSum/main.cpp
+++ Algo/Algorithm/Questions/Arrays/15.fourNumberSum/main.cpp
@@ -1,9 +1,10 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 vector<vector<int>> fourNumberSum(vector<int> array, int targetSum)
 {
-  vector<int> fourNumSum = {};
+  vector<vector<int>> fourNumSum = {};
   for (int i = 0; i < array.size(); i++)
   {
     for (int j = i + 1; j < array.size(); j++)
@@ -13,17 +14,19 @@ vector<vector<int>> fourNumberSum(vector<int> array, int targetSum)
       while (left < right)
       {
         int currSum = array[i] + array[j] + array[left] + array[right];
-        if (currSum < targetSum)
+        if (currSum == targetSum)
+        {
+          fourNumSum.push_back({array[i], array[j], array[left], array[right]});
+          left++;
+          right--;
+        }
+        else if (currSum < targetSum)
         {
           left++;
         }
         else if (currSum > targetSum)
         {
           right--;
-        }
-        else
-        {
-          return {array[i], array[j], array[left], array[right]};
         }
       }
     }
@@ -36,6 +39,10 @@ int main()
 {
   vector<int> array = {7, 6, 4, -1, 1, 2};
   int targetSum = 16;
-  vector<int> result = fourNumberSum(array, targetSum);
+  vector<vector<int>> result = fourNumberSum(array, targetSum);
+  for (vector<int> array : result)
+  {
+    cout << array[0] << " " << array[1] << " " << array[2] << " " << array[3] << endl;
+  }
   return 0;
 }
--- GFG/ProgrammingLanguage/C++/README.md
+++
@@ -1,6 +0,0 @@
-You can run a C++ program by typing in the command line:
-<br>
-
-```cmd
-$ g++ nameOfTheFile.cpp && ./a.out
-```
