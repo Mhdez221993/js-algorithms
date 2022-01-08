500d10cf3f3e2d9506f517df9555bf82b99798a2
Moises <mhdezcoronado@gmail.com>
2022-01-08 20:56:52 +0100

    create a student structure

---
+++ GFG/ProgrammingLanguage/C++/12.storestudentRecords/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct student
+{
+  string id, name;
+  int year;
+  double cgpa;
+};
+
+int main()
+{
+  student team[] = {
+      {"id1", "Robert", 1, 8.9},
+      {"id2", "Karl", 2, 5.9},
+      {"id3", "Peter", 5, 6.9},
+  };
+
+  for (int i = 0; i < 3; i++)
+    cout << "ID: " << team[i].id << " "
+         << "Name: " << team[i].name << " "
+         << "Year: " << team[i].year << " "
+         << "CGPA: " << team[i].cgpa << endl;
+  return 0;
+}
