849d6359a15256aaf0f00cd4efa5505cf879aaf9
Moises <mhdezcoronado@gmail.com>
2022-01-09 22:39:48 +0100

    takes in a list of scores and returns the minimum number of rewards

---
+++ Algo/Algorithm/Questions/Arrays/18.minRewards/main.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int minRewards(vector<int> scores)
+{
+  vector<int> customeScores(scores.size(), 1);
+  int sum = 0;
+
+  for (int i = 1; i < scores.size(); i++)
+  {
+    if (scores[i] > scores[i - 1])
+      customeScores[i] = customeScores[i - 1] + 1;
+  }
+
+  for (int i = (scores.size() - 2); i >= 0; i--)
+  {
+    if (scores[i] > scores[i + 1])
+    {
+      if (customeScores[i] <= customeScores[i + 1])
+        customeScores[i] = customeScores[i + 1] + 1;
+    }
+  }
+
+  for (int x : customeScores)
+    sum += x;
+
+  return sum;
+}
+
+int main()
+{
+  vector<int> array = {8, 4, 2, 1, 3, 6, 7, 9, 5};
+  cout << minRewards(array) << endl;
+  return 0;
+}
