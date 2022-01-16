ba5b64edaa89e134f0d056084a404d37484663c9
Moises <mhdezcoronado@gmail.com>
2022-01-16 16:39:30 +0100

    Optimize waterfallStreams naming

--- Algo/Algorithm/Questions/Arrays/22.waterfallStreams/main.cpp
+++ Algo/Algorithm/Questions/Arrays/22.waterfallStreams/main.cpp
@@ -6,37 +6,39 @@ using namespace std;
 vector<double> waterfallStreams(vector<vector<double>> array, int source)
 {
   int len = array[0].size();
-  vector<double> waterPersentage(len, 0);
-  waterPersentage[source] = 100;
+  vector<double> rowAbove(len);
+  rowAbove[source] = 100;
 
-  for (int i = 0; i < array.size(); i++)
+  for (int row = 0; row < array.size(); row++)
   {
-    vector<double> currWaterPercentage(len, 0);
-    for (int j = 0; j < len; j++)
+    vector<double> currRow(len);
+    for (int col = 0; col < len; col++)
     {
-      if (waterPersentage[j] > 0)
+      double hasWatherAbove = rowAbove[col];
+      bool noBlocker = array[row][col] == 0;
+      if (hasWatherAbove)
       {
-        if (array[i][j] == 0)
-          currWaterPercentage[j] += waterPersentage[j];
+        if (noBlocker)
+          currRow[col] += rowAbove[col];
         else
         {
-          int rightIndx = j;
-          while (rightIndx < len && array[i - 1][rightIndx] != 1)
+          int rightIndx = col;
+          while (rightIndx < len && array[row - 1][rightIndx] != 1)
           {
-            if (array[i][rightIndx] == 0)
+            if (array[row][rightIndx] == 0)
             {
-              currWaterPercentage[rightIndx] += (waterPersentage[j] / 2);
+              currRow[rightIndx] += (rowAbove[col] / 2);
               break;
             }
             rightIndx++;
           }
 
-          int leftIndx = j;
-          while (leftIndx >= 0 && array[i - 1][leftIndx] != 1)
+          int leftIndx = col;
+          while (leftIndx >= 0 && array[row - 1][leftIndx] != 1)
           {
-            if (array[i][leftIndx] == 0)
+            if (array[row][leftIndx] == 0)
             {
-              currWaterPercentage[leftIndx] += (waterPersentage[j] / 2);
+              currRow[leftIndx] += (rowAbove[col] / 2);
               break;
             }
             leftIndx--;
@@ -44,10 +46,10 @@ vector<double> waterfallStreams(vector<vector<double>> array, int source)
         }
       }
     }
-    waterPersentage = currWaterPercentage;
+    rowAbove = currRow;
   }
   // Write your code here.
-  return waterPersentage;
+  return rowAbove;
 }
 
 int main()
