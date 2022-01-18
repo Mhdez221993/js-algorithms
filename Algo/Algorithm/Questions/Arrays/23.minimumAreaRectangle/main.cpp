3041614949d15cb21e8cdaeae64dfe421f7bb69c
Moises <mhdezcoronado@gmail.com>
2022-01-18 15:47:00 +0100

    Find the smollest rectagule

--- Algo/Algorithm/Questions/Arrays/23.minimumAreaRectangle/main.cpp
+++ Algo/Algorithm/Questions/Arrays/23.minimumAreaRectangle/main.cpp
@@ -2,27 +2,47 @@
 #include <climits>
 #include <iostream>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
-unordered_map<int, vector<vector<int>>> getLineBetween2Poins(vector<vector<int>> &points)
+int findAreaOfRectangle(vector<vector<int>> firstSecond, vector<int> third, vector<int> fourth)
 {
-  unordered_map<int, vector<vector<int>>> lines;
-  for (vector<int> pairs : points)
-  {
-    int x = pairs[0];
-    int y = pairs[1];
-    if (lines.find(x) == lines.end())
-      lines[x] = {};
-    lines[x].push_back(pairs);
-  }
-
-  return lines;
+  return 10;
 }
 
 int minimumAreaRectangle(vector<vector<int>> points)
 {
-  int minimumArea = 0;
-  unordered_map<int, vector<vector<int>>> linesBetweenPoints = getLineBetween2Poins(points);
+  int minimumArea = INT_MAX;
+  unordered_map<int, vector<vector<int>>> myHashPoints;
+  for (int i = 0; i < points.size(); i++)
+  {
+    int x = points[i][0];
+    int y = points[i][1];
+    for (int right = i + 1; right < points.size(); right++)
+    {
+      int currX = points[right][0];
+      int currY = points[right][1];
+      int key = max(currY, y) - min(currY, y);
+      if (currX == x && myHashPoints[key].size() > 1)
+      {
+        int currArea = findAreaOfRectangle(myHashPoints[key], points[i], points[right]);
+        minimumArea = min(minimumArea, currArea);
+      }
+    }
+
+    for (int left = i - 1; left >= 0; left--)
+    {
+      int currX = points[left][0];
+      int currY = points[left][1];
+      int key = max(currY, y) - min(currY, y);
+      if (currX == x)
+        myHashPoints[key] = {points[i], points[left]};
+    }
+  }
+
+  if (minimumArea == INT_MAX)
+    return 0;
+
   return minimumArea;
 }
 
