ae856cd8a23ca1df64520251bf933ccdefcd1cc1
Moises <mhdezcoronado@gmail.com>
2022-01-19 21:36:34 +0100

    Sort a unordered map by value and keys

--- Algo/Algorithm/Questions/Arrays/23.minimumAreaRectangle/main.cpp
+++ Algo/Algorithm/Questions/Arrays/23.minimumAreaRectangle/main.cpp
@@ -1,53 +1,45 @@
 #include <vector>
 #include <climits>
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <unordered_map>
 #include <algorithm>
 using namespace std;
-// string key = concat(to_string(min(currY, y)), to_string(max(currY, y)));
 
-int findAreaOfRectangle(int len, int width1, int width2)
+unordered_map<int, vector<int>> sortColumns(vector<vector<int>> points)
 {
-  int width = max(width1, width2);
-  cout << (width * len) << endl;
-  return len * width;
-}
-
-int minimumAreaRectangle(vector<vector<int>> points)
-{
-  int minimumArea = INT_MAX;
-  unordered_map<int, vector<vector<int>>> myHashPoints;
+  unordered_map<int, vector<int>> columns;
   for (int i = 0; i < points.size(); i++)
   {
     int x = points[i][0];
     int y = points[i][1];
-    for (int right = i + 1; right < points.size(); right++)
-    {
-      int currX = points[right][0];
-      int currY = points[right][1];
-      int key = max(currY, y) - min(currY, y);
-      if (currX == x && myHashPoints[key].size() > 1)
-      {
-        int currArea = findAreaOfRectangle(key, currX, myHashPoints[key][0][0]);
-        minimumArea = min(minimumArea, currArea);
-      }
-    }
-
-    for (int left = i - 1; left >= 0; left--)
-    {
-      int currX = points[left][0];
-      int currY = points[left][1];
-      int key = max(currY, y) - min(currY, y);
-      if (currX == x)
-        myHashPoints[key] = {points[i], points[left]};
-    }
+    if (columns.find(x) == columns.end())
+      columns[x] = {};
+    columns[x].push_back(y);
   }
 
-  if (minimumArea == INT_MAX)
-    return 0;
+  return columns;
+}
 
-  return minimumArea;
+vector<int> sortKeys(unordered_map<int, vector<int>> points)
+{
+  vector<int> sortKeys;
+  for (auto x : points)
+  {
+    int key = x.first;
+    if (points[key].size() > 1)
+      sortKeys.push_back(key);
+  }
+
+  sort(sortKeys.begin(), sortKeys.end());
+  return sortKeys;
+}
+
+int minimumAreaRectangle(vector<vector<int>> points)
+{
+
+  unordered_map<int, vector<int>> sortedColumns = sortColumns(points);
+  vector<int> sortedKeys = sortKeys(sortedColumns);
 }
 
 int main()
