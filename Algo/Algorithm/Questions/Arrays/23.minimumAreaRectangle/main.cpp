67c91098dc522da78321e0b136237658fa56c53e
Moises <mhdezcoronado@gmail.com>
2022-01-20 15:50:18 +0100

    Find area of a rectangle

--- Algo/Algorithm/Questions/Arrays/23.minimumAreaRectangle/main.cpp
+++ Algo/Algorithm/Questions/Arrays/23.minimumAreaRectangle/main.cpp
@@ -1,59 +1,116 @@
 #include <vector>
-#include <climits>
 #include <iostream>
-#include <string.h>
 #include <unordered_map>
+#include <climits>
 #include <algorithm>
+// #include <string>
 using namespace std;
 
-unordered_map<int, vector<int>> sortColumns(vector<vector<int>> points)
+unordered_map<int, vector<int>> mapPoinst(vector<vector<int>> points)
 {
   unordered_map<int, vector<int>> columns;
-  for (int i = 0; i < points.size(); i++)
+  for (vector<int> point : points)
   {
-    int x = points[i][0];
-    int y = points[i][1];
+    int x = point[0];
+    int y = point[1];
     if (columns.find(x) == columns.end())
       columns[x] = {};
     columns[x].push_back(y);
   }
 
+  for (auto column : columns)
+  {
+    int x = column.first;
+    vector<int> y = column.second;
+    sort(y.begin(), y.end());
+    columns[x] = y;
+  }
+
   return columns;
 }
 
-vector<int> sortKeys(unordered_map<int, vector<int>> points)
+vector<int> keysFromColums(unordered_map<int, vector<int>> columns)
 {
   vector<int> sortKeys;
-  for (auto x : points)
+  for (auto column : columns)
   {
-    int key = x.first;
-    if (points[key].size() > 1)
-      sortKeys.push_back(key);
+    int x = column.first;
+    if (columns[x].size() > 1)
+      sortKeys.push_back(x);
   }
-
   sort(sortKeys.begin(), sortKeys.end());
+
   return sortKeys;
+}
+
+string intToSting(int y1, int y2)
+{
+  string key = to_string(y1) + "," + to_string(y2);
+  return key;
+}
+
+vector<int> stringToIng(string str)
+{
+  int index = str.find(",");
+  int first = stoi(str.substr(0, index));
+  int second = stoi(str.substr(index + 1, str.length()));
+  return {first, second};
+}
+
+int getArea(int x1, int x2, int y1, int y2)
+{
+  int len = x2 - x1;
+  int width = y2 - y1;
+  return len * width;
 }
 
 int minimumAreaRectangle(vector<vector<int>> points)
 {
+  int minimumArea = INT_MAX;
+  unordered_map<int, vector<int>> columns = mapPoinst(points);
+  vector<int> sortedKeys = keysFromColums(columns);
+  unordered_map<string, int> edges;
 
-  unordered_map<int, vector<int>> sortedColumns = sortColumns(points);
-  vector<int> sortedKeys = sortKeys(sortedColumns);
+  for (int i = 0; i < sortedKeys.size(); i++)
+  {
+    int x = sortedKeys[i];
+    vector<int> currentCol = columns[x];
+    for (int j = 1; j < currentCol.size(); j++)
+    {
+      int left = 0;
+      int currY = currentCol[j];
+      while (left >= 0)
+      {
+        int prevY = currentCol[left];
+        string key = intToSting(prevY, currY);
+        if (edges.find(key) != edges.end())
+        {
+          int edgesKey = edges[key];
+          vector<int> values = stringToIng(key);
+          int currArea = getArea(edgesKey, x, values[0], values[1]);
+          minimumArea = min(currArea, minimumArea);
+        }
+        edges[key] = x;
+        left--;
+      }
+    }
+  }
+
+  if (minimumArea == INT_MAX)
+    return 0;
+  return minimumArea;
 }
 
 int main()
 {
   vector<vector<int>> points = {
-      {1, 5},
-      {5, 1},
+      {-4, 4},
+      {4, 4},
+      {4, -2},
+      {-4, -2},
+      {0, -2},
       {4, 2},
-      {2, 4},
-      {2, 2},
-      {1, 2},
-      {4, 5},
-      {2, 5},
-      {-1, -2}};
+      {0, 2}};
   cout << minimumAreaRectangle(points) << endl;
   return 0;
 }
