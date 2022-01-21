c4dfbf1caaf561fb883d522a5a914b4c1758cd06
Moises <mhdezcoronado@gmail.com>
2022-01-21 14:18:09 +0100

    Remove redundant code

--- Algo/Algorithm/Questions/Arrays/23.minimumAreaRectangle/main.cpp
+++ Algo/Algorithm/Questions/Arrays/23.minimumAreaRectangle/main.cpp
@@ -18,88 +18,44 @@ unordered_map<int, vector<int>> mapPoinst(vector<vector<int>> points)
     columns[x].push_back(y);
   }
 
-  for (auto column : columns)
-  {
-    int x = column.first;
-    vector<int> y = column.second;
-    sort(y.begin(), y.end());
-    columns[x] = y;
-  }
-
   return columns;
-}
-
-vector<int> keysFromColums(unordered_map<int, vector<int>> columns)
-{
-  vector<int> sortKeys;
-  for (auto column : columns)
-  {
-    int x = column.first;
-    if (columns[x].size() > 1)
-      sortKeys.push_back(x);
-  }
-  sort(sortKeys.begin(), sortKeys.end());
-
-  return sortKeys;
-}
-
-string intToSting(int y1, int y2)
-{
-  string key = to_string(y1) + "," + to_string(y2);
-  return key;
-}
-
-vector<int> stringToIng(string str)
-{
-  int index = str.find(",");
-  int first = stoi(str.substr(0, index));
-  int second = stoi(str.substr(index + 1, str.length()));
-  return {first, second};
-}
-
-int getArea(int x1, int x2, int y1, int y2)
-{
-  int len = x2 - x1;
-  int width = y2 - y1;
-  return len * width;
 }
 
 int minimumAreaRectangle(vector<vector<int>> points)
 {
   int minimumArea = INT_MAX;
   unordered_map<int, vector<int>> columns = mapPoinst(points);
-  vector<int> sortedKeys = keysFromColums(columns);
+  vector<int> sortedKeys;
   unordered_map<string, int> edges;
+
+  for (auto x : columns)
+    sortedKeys.push_back(x.first);
+  sort(sortedKeys.begin(), sortedKeys.end());
 
   for (int i = 0; i < sortedKeys.size(); i++)
   {
-    int x = sortedKeys[i];
-    vector<int> currentCol = columns[x];
-    for (int j = 1; j < currentCol.size(); j++)
+    int x2 = sortedKeys[i];
+    vector<int> currentCol = columns[x2];
+    sort(currentCol.begin(), currentCol.end());
+    for (int currIndx = 0; currIndx < currentCol.size(); currIndx++)
     {
-      int left = j - 1;
-      int currY = currentCol[j];
-      while (left >= 0)
+      int y2 = currentCol[currIndx];
+      for (int prevIndex = 0; prevIndex < currIndx; prevIndex++)
       {
-        int prevY = currentCol[left];
-        string key = intToSting(prevY, currY);
-        if (edges.find(key) != edges.end())
+        int y1 = currentCol[prevIndex];
+        string pointKey = to_string(y1) + ":" + to_string(y2);
+        if (edges.find(pointKey) != edges.end())
         {
-          int edgesKey = edges[key];
-          vector<int> values = stringToIng(key);
-          int currArea = getArea(edgesKey, x, values[0], values[1]);
+          int x1 = edges[pointKey];
+          int currArea = ((x2 - x1) * (y2 - y1));
           minimumArea = min(currArea, minimumArea);
         }
-        edges[key] = x;
-        left--;
+        edges[pointKey] = x2;
       }
     }
-    cout << endl;
   }
 
-  if (minimumArea == INT_MAX)
-    return 0;
-  return minimumArea;
+  return minimumArea == INT_MAX ? 0 : minimumArea;
 }
 
 int main()
