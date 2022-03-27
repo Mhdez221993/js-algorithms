3561ae857eaa805fdc7e0a4d4c6ef27e50a187a1
Moises <mhdezcoronado@gmail.com>
2022-03-28 00:41:57 +0200

    Optimize find the closest Value in a BST

--- practice/algorithms/challenges/02.binary_search_threes/01.find_closest_value_in_bst/main.js
+++ practice/algorithms/challenges/02.binary_search_threes/01.find_closest_value_in_bst/main.js
@@ -1,16 +1,17 @@
-let closest = Number.MAX_SAFE_INTEGER
-
-function findClosestValueInBst(tree, target) {
-  if (tree) {
-    let curr = tree.value < target ? (target - tree.value) : (tree.value - target)
-    let prev = closest < target ? (target - closest) : (closest - target)
-    if (curr < prev) closest = tree.value
-
-    findClosestValueInBst(tree.left, target)
-    findClosestValueInBst(tree.right, target)
+function findClosestValueInBstHelper(tree, target, closest) {
+  if (!tree) {
+    return closest
   }
 
-  return closest
+  if (Math.abs(tree.value - target) < Math.abs(closest - target)) {
+    return findClosestValueInBstHelper(tree.left, target, tree.value)
+  }
+  return findClosestValueInBstHelper(tree.right, target, closest)
+
+}
+
+function findClosestValueInBst(tree, target) {
+  return findClosestValueInBstHelper(tree, target, tree.value)
 }
 
 // This is the class of the input tree. Do not edit.
