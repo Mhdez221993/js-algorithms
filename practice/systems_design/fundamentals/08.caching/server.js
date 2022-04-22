d9690b5aa07882b7ba37c2acb98b9298416a4c2f
Moises <mhdezcoronado@gmail.com>
2022-04-22 19:24:05 +0200

    Define Caching

---
+++ practice/systems_design/fundamentals/08.caching/server.js
@@ -0,0 +1,25 @@
+const database = require('./database');
+const express = require('express');
+
+const app = express();
+const cache = {};
+
+app.get('/nocache/index.html', (req, res) => {
+  database.get('index.html', page => {
+    res.send(page);
+  })
+});
+
+app.get('/withcache/index.html', (req, res) => {
+  if ('index.html' in cache) {
+    res.send(cache['index.html']);
+    return;
+  }
+
+  database.get('index.html', page => {
+    cache['index.html'] = page;
+    res.send(page);
+  });
+})
+
+app.listen(3000, () => console.log('Listening on post 3000'));
