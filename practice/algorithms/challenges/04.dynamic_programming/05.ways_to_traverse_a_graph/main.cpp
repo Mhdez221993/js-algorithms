328ed4419cdb3d34bf4ad97d08e34f19b4844d5c
Moises <mhdezcoronado@gmail.com>
2022-05-21 16:10:19 +0200

    solve number of ways to traverse a graph iterative

--- practice/algorithms/challenges/04.dynamic_programming/05.ways_to_traverse_a_graph/main.cpp
+++ practice/algorithms/challenges/04.dynamic_programming/05.ways_to_traverse_a_graph/main.cpp
@@ -9,3 +9,21 @@ using namespace std;
 
 //   return numberOfWaysToTraverseGraph(width - 1, height) + numberOfWaysToTraverseGraph(width, height - 1);
 // }
+
+// O((n*m)) time | O(n * m) space
+int numberOfWaysToTraverseGraph(int width, int height)
+{
+  vector<vector<int>> numberOfWays(height, vector<int>(width, 1));
+
+  for (int row = 1; row < height; row++)
+  {
+    for (int col = 1; col < width; col++)
+    {
+      int left = numberOfWays[row][col - 1];
+      int up = numberOfWays[row - 1][col];
+      numberOfWays[row][col] = left + up;
+    }
+  }
+
+  return numberOfWays[height - 1][width - 1];
+}
