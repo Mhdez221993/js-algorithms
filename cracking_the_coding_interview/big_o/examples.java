b64eddec3ed00effcd32c4c790a5b1e8b8ac9338
Moises <mhdezcoronado@gmail.com>
2022-10-03 17:41:25 +0200

    Add O(ab) example

--- cracking_the_coding_interview/big_o/examples.java
+++ cracking_the_coding_interview/big_o/examples.java
@@ -38,3 +38,27 @@ void printUnorderedPairs(int[] array) {
 
 // Example 4
 // This is similar to the above, but now we have two different arrays.
+void printUnorderedPairs (int[] arrayA, int[] arrayB) {
+  for (int i = 0; i < arrayA.length; i++) {
+    for (int j = 0; j < arrayB.length; j++) {
+      if (arrayA[i] < arrayB.length; j++) {
+        System.out.println(arrayA[i] + ", " + arrayB[j]);
+      }
+    }
+  }
+}
+// For each element of array A, the inner for loop goes through b iterations, where b = arrayB. length.
+// lf a = arrayA.length,then the runtime is O(ab).
+
+// Examples 5
+// What about this strange bit of code?
+void printUnorderedPairs (int[] arrayA, int[] arrayB) {
+  for (int i = 0; i < arrayA.length; i++) { // O(a)
+    for (int j = 0; j < arrayB.length; j++) { // O(b)
+      for (int k = 0; k < 100000; k++) { // O(1)
+        System.out.println(arrayA[i] + "," arrayB[j]); // O(1)
+      }
+    }
+  }
+}
+// Nothing has really changed here. 100,000 units of work is still constant, so the runtime is 0(ab).
