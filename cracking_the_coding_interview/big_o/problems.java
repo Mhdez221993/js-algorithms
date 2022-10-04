bc3d551a01cbb79ecf66cda18c2cdf3f3a05ad84
Moises <mhdezcoronado@gmail.com>
2022-10-04 23:06:13 +0200

    Compute the integer square of a number

--- cracking_the_coding_interview/big_o/problems.java
+++ cracking_the_coding_interview/big_o/problems.java
@@ -64,3 +64,16 @@ int sqrt_helper(int n, int min, int max) {
   }
 }
 // O(log n)
+
+// The following code computes the [integer] square root of a number. If the number is not
+// a perfect square (there is no integer square root), then it returns -1. It does this by trying
+// increasingly large numbers until it finds the right value (or is too high). What is its runtime?
+int sqrt(int n) {
+  for (int guess = 1; guess * guess <= n; guess++) {
+    if (guess * guess == n) {
+      return guess;
+    }
+  }
+  return -1;
+}
+// O(log n)
