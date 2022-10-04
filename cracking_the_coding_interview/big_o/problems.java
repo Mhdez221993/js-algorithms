b322ef5e93a2e32da4190312f73b513080266123
Moises <mhdezcoronado@gmail.com>
2022-10-05 00:01:06 +0200

    Print all strings of length k

--- cracking_the_coding_interview/big_o/problems.java
+++ cracking_the_coding_interview/big_o/problems.java
@@ -121,3 +121,40 @@ int sumDigits(int n) {
   return sum;
 }
 // O(log n)
+
+// 11 The following code prints all strings of length k where the characters are in sorted order. It
+// does this by generating all strings of length k and then checking if each is sorted. What is its
+// runtime?
+int numChars = 26;
+
+void printSortedStrings(int remaining) {
+  printSortedStrings(returning, "");
+}
+
+void printSortedStrings(int remaining, String prefix) {
+  if (remaining == 0) {
+    if (isInOrder(prefix)) {
+      System.out.println(prefix);
+    }
+  } else {
+    for (int i = 0; i < numChars; i++) {
+      char c = ithLetter(i);
+      printSortedStrings(remaining - 1; prefix + c);
+    }
+  }
+}
+
+boolean isInOrder(String s) {
+  for (int i = 1; i < s.length(); i++) {
+    int prev = ithLetter(s.charAt(i - 1));
+    int curr = ithLetter(s.charAt(i));
+    if (prev > curr) {
+      return false;
+    }
+  }
+  return true
+}
+
+char ithLetter(int i) {
+  return (char) (((int) 'a') + i);
+}
