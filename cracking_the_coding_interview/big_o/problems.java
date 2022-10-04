fdbd0d4e0a5b0f21ada6dbe3b846838df2ca5049
Moises <mhdezcoronado@gmail.com>
2022-10-04 22:59:23 +0200

    Compute the integer square of a number

--- cracking_the_coding_interview/big_o/problems.java
+++ cracking_the_coding_interview/big_o/problems.java
@@ -30,7 +30,7 @@ int mod(int a, int b) {
 }
 // O(1)
 
-// The following code performs integer division. What is its runtime (assume a and b are both
+// 4 The following code performs integer division. What is its runtime (assume a and b are both
 // positive)?
 int div(int a, int b) {
   int count = 0; // O(1)
@@ -41,3 +41,26 @@ int div(int a, int b) {
   }
   return count;
 }
+// O(log b * a)
+
+// 5 The following code computes the [integer] square root of a number. If the number is not a
+// perfect square (there is no integer square root), then it returns -1. It does this by successive
+// guessing. If n is 100, it first guesses 5O. Too high? Try something lower - halfway between 1
+// and 5O. What is its runtime?
+int sqrt(int n) {
+  return sqrt_helper(n, 1, n);
+}
+
+int sqrt_helper(int n, int min, int max) {
+  if (max < min) return -1; // no square root
+
+  int guess = (min + max) / 2;
+  if (guess * guess == n) { // found it!
+    return guess;
+  } else if (guess * guess < n) { // too low
+    return sqrt_helper(n, guess + 1, max); // try  higher
+  } else {
+    return sqrt_helper(n, min, guess - 1); // try lower
+  }
+}
+// O(log n)
