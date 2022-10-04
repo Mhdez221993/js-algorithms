3bcf7ba04ae13e4f1b007fb70deb9c8a50092a4b
Moises <mhdezcoronado@gmail.com>
2022-10-04 20:00:17 +0200

    Print all Fibonacci numbers form 0 to n

--- cracking_the_coding_interview/big_o/examples.java
+++ cracking_the_coding_interview/big_o/examples.java
@@ -178,7 +178,8 @@ int fib (int n) {
 // We can use the earlier pattern we'd established for recursive calls: O(branches^depth).
 // There are 2 branches per call, and we go as deep as N, therefore the runtime is O(2^N).
 
-// The following code prints all Fibonacci numbers from Oto n. What is its time complexity?
+// Example 14
+// The following code prints all Fibonacci numbers from O to n. What is its time complexity?
 void allFib(int n) {
   for (int i = 0; i < n; i++) {
     System.out.println(i + ": " + fib(i));
@@ -190,3 +191,31 @@ int fib(int n) {
   else if (n == 1) return 1;
   return fib(n - 1) + fib(n - 2)
 }
+// The runtime to compute the first n Fibonacci numbers
+// (using this terrible algorithm) is still O ( 2°).
+
+// Example 15
+// The following code prints all Fibonacci numbers from O to n.
+// However, this time, it stores (i.e., caches) previously computed values in an integer array.
+// If it has already been computed, it just returns the cache. What
+// is its runtime?
+void allFib(int n) {
+  int[] memo = new int[n + 1];
+  for (int i = 0; i < n; i++) {
+    System.out.println(i + ": " + fib(i, memo));
+  }
+}
+
+int fib(int n, int[] memo) {
+  if (n <= 0) return 0;
+  else if (n == 1) return 1;
+  else if (memo[n] > 0) return memo[n];
+
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+}
+// At each call to fib(i), we have already computed and stored the values for fib(i-1) and fib(i-2).
+// We just look up those values, sum them, store the new result, and return. This takes a constant amount of
+// time.
+// We're doing a constant amount of work N times, so this is O (n) time.
+// This technique, called memoization, is a very common one to optimize exponential time recursive algorithms.
