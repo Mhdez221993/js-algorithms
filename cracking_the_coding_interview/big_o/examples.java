8d6ed21cfd647aa67ed1e4a01495763b1eb79893
Moises <mhdezcoronado@gmail.com>
2022-10-04 18:55:40 +0200

    Count the permutations of a string

--- cracking_the_coding_interview/big_o/examples.java
+++ cracking_the_coding_interview/big_o/examples.java
@@ -147,4 +147,23 @@ int factorial (int n) {
     return n * factorial(n - 1);
   }
 }
-// This is just a straight recursion from n to n -1 to n - 2 down to 1. It will take O ( n) time
+// This is just a straight recursion from n to n-1 to n-2 down to 1. It will take O ( n) time
+
+// Example 12
+// This code counts all permutations of a string.
+void permutation (String str) {
+  permutation(str, "");
+}
+
+void permutation (String str, String prefix) {
+  if (str.length() == 0) {
+    System.out.println(prefix);
+  } else {
+    for (int i = 0; i < str.length(); i++) {
+      String rem = str.substring(0, i) + str.substring(i + 1);
+      permutation(rem, prefix + str.charAt(i));
+    }
+  }
+}
+// Since we are calling permutation 0(n * n!) times (as an upper bound), and each one takes 0(n) time,
+// the total runtime will not exceed O(n^2 * n!)
