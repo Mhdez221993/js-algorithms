23176d9cff175fb2e99fd726b981d1036e2bfc8f
Moises <mhdezcoronado@gmail.com>
2022-10-07 22:13:56 +0200

    Add isUnique first solution

--- practice/algorithms/challenges/01.arrays_and_strings/32.is_unique/main.java
+++ practice/algorithms/challenges/01.arrays_and_strings/32.is_unique/main.java
@@ -1,2 +1,20 @@
 // 1. Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
 // cannot use additional data structures?
+
+/*
+build a hash table save every character init
+Set the caracter to equal 0 if does not exist else return false
+*/
+import java.util.*;
+
+public boolean isUnique(String str) {
+  Dictionary unique = new Hashtable();
+  for(chart c: str) {
+    if(!unique[c]) {
+      unique[c] = '0';
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
