b0fe863c58cb1892684dfdbc0d5c23c40bdd72a8
Moises <mhdezcoronado@gmail.com>
2022-10-10 20:06:29 +0200

    Build pascal triangle

--- preparation/programming_language/java/HelloWorld.java
+++ preparation/programming_language/java/HelloWorld.java
@@ -1,16 +1,22 @@
 // package ch01.sec01;
-import java.util.Random;
+import java.util.*;
+
 
 // Our first Java program
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!".length());
-
-        Random generator = new Random();
-        System.out.println(generator.nextInt());
-        System.out.println(generator.nextInt());
-        System.out.println(Math.floorMod(10 + -5, 12));
+        int n = 5;
+        int[][] triangle = new int[n][];
+        for (int i = 0; i < n; i++) {
+            triangle[i] = new int[i + 1];
+            triangle[i][0] = 1;
+            triangle[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+        System.out.println(Arrays.deepToString(triangle));
     }
+
 }
