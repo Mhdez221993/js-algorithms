7ae778d14d9ef071902c43da946c2764a4a6a995
Moises <mhdezcoronado@gmail.com>
2022-10-12 21:46:13 +0200

    Runtime information

--- preparation/programming_language/java/HelloWorld.java
+++ preparation/programming_language/java/HelloWorld.java
@@ -6,17 +6,8 @@ import java.util.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        int n = 5;
-        int[][] triangle = new int[n][];
-        for (int i = 0; i < n; i++) {
-            triangle[i] = new int[i + 1];
-            triangle[i][0] = 1;
-            triangle[i][i] = 1;
-            for (int j = 1; j < i; j++) {
-                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-            }
-        }
-        System.out.println(Arrays.deepToString(triangle));
+        HelloWorld hello = new HelloWorld();
+        System.out.println(hello.getClass().getName());
     }
 
 }
