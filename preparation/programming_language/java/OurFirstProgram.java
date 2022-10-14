7eb893de4453f65b5e43321fa951e2a708a121ce
Moises <mhdezcoronado@gmail.com>
2022-10-14 09:18:47 +0200

    Create a generic class

--- preparation/programming_language/java/OurFirstProgram.java
+++ preparation/programming_language/java/OurFirstProgram.java
@@ -1,5 +1,13 @@
-public class OurFirstProgram {
-  public static void main (String[] args) {
-    System.out.println("Hello World!");
+public class Entry<k, V> {
+  private K key;
+  private V value;
+
+  public Entry (K key, V value) {
+    this.key = key;
+    this.value = value;
   }
+
+  public k getKey() { return key; }
+  public v getValue() { return value; }
 }
+
