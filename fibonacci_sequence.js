18d43d4b616eddfa866dc51eb0b36387b506c4a9
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-03 13:31:28 +0100

    Fibonacci with recurson

--- fibonacci_sequence.js
+++ fibonacci_sequence.js
@@ -1,17 +1,19 @@
 function fibonacci(n) {
-  // O(1)
   let fib = [0, 1];
 
-  // O(n)
   for (let i = 2; i < n; i++) {
-    // O(1)
     fib[i] = fib[i - 1] + fib[i - 2];
   }
 
-  // O(1)
   return fib;
 }
-
 // Big O(n)
+// console.log(fibonacci(7));
 
-console.log(fibonacci(7));
+function fib(n) {
+  if (n < 2) return n;
+
+  return fib(n - 1) + fib(n - 2);
+}
+// Big O(2^n)
+console.log(fib(6));
