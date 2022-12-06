a17c249b22546fdc0fcd44016ee71eeec2aba88e
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-06 20:55:33 +0100

    Insertion sort

---
+++ insertion_sort.js
@@ -0,0 +1,17 @@
+function insertion_sort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let j = i - 1;
+    let value_to_insert = array[i];
+
+    while (j >= 0 && arr[j] > value_to_insert) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = value_to_insert;
+  }
+
+  return array;
+}
+
+let arr = [8, 20, -2, 4, -6];
+console.log(insertion_sort(arr));
