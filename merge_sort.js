7e549de30274b864d93f2aefb7b3cea0d1a318b6
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-08 17:52:14 +0100

    Finish merge_sort

--- merge_sort.js
+++ merge_sort.js
@@ -1,15 +1,16 @@
-function merge_sort(arr) {
+function mege_sort(arr) {
   if (arr.length < 2) return arr;
 
   const mid = Math.floor(arr.length / 2);
-  const letf = arr.slice(0, mid);
-  const rigth = arr.slice(mid);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
 
-  return merge(merge_sort(letf), merge_sort(rigth));
+  return merge(mege_sort(left), mege_sort(right));
 }
 
 function merge(leftArr, rightArr) {
-  let sortedArr = [];
+  const sortedArr = [];
+
   while (leftArr.length && rightArr.length) {
     if (leftArr[0] <= rightArr[0]) {
       sortedArr.push(leftArr.shift());
@@ -22,4 +23,4 @@ function merge(leftArr, rightArr) {
 }
 
 const arr = [8, 20, -2, 4, -6];
-console.log(merge_sort(arr));
+console.log(mege_sort(arr));
