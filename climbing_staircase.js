c6a35393c9cdfc4c69f5548e235d91ff536874a5
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-08 18:27:25 +0100

    Climbing staircase iterative

--- climbing_staircase.js
+++ climbing_staircase.js
@@ -4,4 +4,13 @@ function climbing_staircase(n) {
   return climbing_staircase(n - 1) + climbing_staircase(n - 2);
 }
 
+function climbing_staircase_with_loop(n) {
+  const stairs = [1, 2];
+  for (let i = 2; i <= n; i++) {
+    stairs[i] = stairs[i - 1] + stairs[i - 2];
+  }
+  return stairs[n - 1];
+}
+
 console.log(climbing_staircase(4));
+console.log(climbing_staircase_with_loop(4));
