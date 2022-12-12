262d99698040a8429f3b13a8c55afe0d3bb0c92e
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-12 18:35:01 +0100

    Implement Quueue with array

---
+++ queue_with_array.js
@@ -0,0 +1,42 @@
+class Queue {
+  constructor(items = []) {
+    this.items = items;
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  peek() {
+    if (!this.isEmpty) {
+      return this.items[0];
+    }
+    return null;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+const queue = new Queue();
+console.log(queue.isEmpty());
+
+queue.enqueue(1);
+queue.enqueue(10);
+queue.enqueue(100);
+
+queue.dequeue();
+queue.print();
