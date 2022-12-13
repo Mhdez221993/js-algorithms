f2d81957af1b6adb0fb87db3a733511dba58c2da
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-13 18:57:50 +0100

    Implement prepend method for linked list

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -1,6 +1,7 @@
 class Node {
-  constructor() {
-    this.value = null;
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
@@ -17,9 +18,29 @@ class LinkedList {
   getZise() {
     return this.size;
   }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  print() {
+    console.log(this.head);
+  }
 }
 
 const list = new LinkedList();
 
 console.log(list.getZise());
 console.log(list.isEmpty());
+
+list.prepend(10);
+list.prepend(20);
+
+list.print();
