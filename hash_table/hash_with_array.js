5a97421f43d9139f8949c666b48a0821f2e91973
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-15 17:06:19 +0100

    Hash table implementation

---
+++ hash_table/hash_with_array.js
@@ -0,0 +1,48 @@
+class HashTable {
+  constructor(size) {
+    this.table = new Array(size);
+    this.size = size;
+  }
+
+  hash(key) {
+    let index = 0;
+    for (let i = 0; i < key.length; i++) {
+      index += key.charCodeAt(i);
+    }
+    return index % this.size;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    this.table[index] = value;
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    return this.table[index];
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    this.table[index] = undefined;
+  }
+
+  display() {
+    for (let i = 0; i < this.size; i++) {
+      if (this.table[i]) {
+        console.log(i, this.table[i]);
+      }
+    }
+    console.log("");
+  }
+}
+
+const table = new HashTable(5);
+
+table.set("name", "Bruce");
+table.set("age", 40);
+
+table.display();
+
+table.set("name", "Clark");
+table.display();
