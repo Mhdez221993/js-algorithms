da2df63f99322df7a3f340a0cd4d99af85b19eb3
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-15 18:24:55 +0100

    Define a BST and Node

---
+++ binary_search_tree/bst.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root === null;
+  }
+}
+
+const bst = new BinarySearchTree();
+console.log(bst.isEmpty());
