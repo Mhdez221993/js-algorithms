6d3be8571796594610e37fe163cec7dca19b3804
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-15 23:36:42 +0100

    Adjacency list of a graph

--- graph/graph.js
+++ graph/graph.js
@@ -6,10 +6,18 @@
 
 */
 
+// Adjacency matix of a graph
 const matrix = [
   [0, 1, 0],
   [1, 0, 1],
   [0, 1, 0],
 ];
-
 console.log(matrix[0][1]);
+
+// Adjacency list of a graph
+const adjacencyList = {
+  A: ["B"],
+  B: ["A", "C"],
+  C: ["B"],
+};
+console.log(adjacencyList["B"]);
