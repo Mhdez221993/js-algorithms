a58a16174e7c57d4da880bf66717febb7d92bced
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-15 18:48:56 +0100

    BST insert method

--- binary_search_tree/bst.js
+++ binary_search_tree/bst.js
@@ -14,7 +14,45 @@ class BinarySearchTree {
   isEmpty() {
     return this.root === null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(root, node) {
+    if (node.value < root.value) {
+      if (root.left === null) {
+        root.left = node;
+      } else {
+        this.insertNode(root.left, node);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = node;
+      } else {
+        this.insertNode(root.right, node);
+      }
+    }
+  }
+
+  display() {
+    console.log(this.root);
+  }
 }
 
 const bst = new BinarySearchTree();
 console.log(bst.isEmpty());
+
+bst.insert(5);
+bst.insert(3);
+bst.insert(7);
+bst.insert(2);
+bst.insert(4);
+
+bst.display();
